8a34d93d3547a06749f970b8e7a470b32dd8a972
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-09 20:33:32 +0200

    refactor: :construction: crea navbar
    crea il componente navbar

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,15 @@
+import {
+    Navbar, 
+    NavbarBrand, 
+    NavbarContent, 
+    NavbarItem, 
+    NavbarMenuToggle,
+    NavbarMenu,
+    NavbarMenuItem
+  } from "@nextui-org/react";
+
+const NavbarSample = () => {
+  return <Navbar >Navbar</Navbar>;
+};
+
+export default NavbarSample;
