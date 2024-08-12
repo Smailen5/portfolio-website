0aa70f30af0752082bfead1c88bdb35dd97fc811
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-12 17:10:02 +0200

    refactor: :lipstick: aggiunge icone
    aggiunge icone per ogni skills

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -2,6 +2,8 @@ import { Button } from "@nextui-org/react";
 import { IoLogoJavascript } from "react-icons/io5";
 import { IoLogoReact } from "react-icons/io5";
 import { RiTailwindCssFill } from "react-icons/ri";
+import { FaGitAlt } from "react-icons/fa";
+import { SiNextui } from "react-icons/si";
 // import { RiJavascriptFill } from "react-icons/ri";
 const Skills = () => {
   return (
@@ -12,30 +14,23 @@ const Skills = () => {
           JavaScript
           <IoLogoJavascript color="yellow" size={24} className="bg-black" />
         </div>
-        <div className="flex items-center justify-center bg-green-300 p-4">
+        <div className="flex items-center justify-between bg-green-300 p-4">
           tailwind
           <RiTailwindCssFill size={24} className="fill-cyan-500" />
         </div>
-        <div className="flex items-center justify-center bg-green-300 p-4">
+        <div className="flex items-center justify-between bg-green-300 p-4">
           React
-          <IoLogoReact
-            color="blue"
-            size={24}
-            className="animate-spin-slow"
-          />
+          <IoLogoReact color="blue" size={24} className="animate-spin-slow" />
         </div>
-        <div className="flex items-center justify-center bg-green-300 p-4">
+        <div className="flex items-center justify-between bg-green-300 p-4">
           Git
-          <IoLogoJavascript color="yellow" size={24} className="bg-black" />
+          <FaGitAlt size={24} className="fill-orange-400" />
         </div>
-        <div className="flex items-center justify-center bg-green-300 p-4">
+        <div className="flex items-center justify-between bg-green-300 p-4">
           NextUI
-          <IoLogoJavascript color="yellow" size={24} className="bg-black" />
+          <SiNextui size={24} className="" />
         </div>
-        <div className="flex items-center justify-center bg-green-300 p-4">
-          NextUI
-          <IoLogoJavascript color="yellow" size={24} className="bg-black" />
-        </div>
+        
         <div className="col-span-3 col-start-1">
           <p className="text-center">Guarda i miei lavori</p>
           <Button className="w-full">Portfolio</Button>
