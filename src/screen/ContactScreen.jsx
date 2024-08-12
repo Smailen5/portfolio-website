8d55f40a835e44dec18e2ec35a5a5ee29133e53b
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-12 19:55:48 +0200

    refactor: :iphone: cambia flexbox
    sposta flexbox su schermi xl

--- src/screen/ContactScreen.jsx
+++ src/screen/ContactScreen.jsx
@@ -1,11 +1,13 @@
+import FormComponent from "../Components/FormComponent";
 import NavbarSample from "../Components/Navbar";
 
 const ContactScreen = () => {
   return (
     <>
       <NavbarSample />
-      <div className="min-h-screen bg-red-300 py-16">
-        <div>ContactScreen</div>
+      <div className="mt-20 flex min-h-screen flex-col items-center gap-8 bg-red-300 py-16 xl:max-w-[1024px]">
+        <h3>Contattami</h3>
+        <FormComponent />
       </div>
     </>
   );
