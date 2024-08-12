ff5169bc876fa0a73580b318e5a8dadd7a2531a1
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-12 17:04:06 +0200

    refactor: :lipstick: aggiunge animazione
    aggiunge animazione spin-slow per l'icona di react

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,27 +1,40 @@
 import { Button } from "@nextui-org/react";
-
+import { IoLogoJavascript } from "react-icons/io5";
+import { IoLogoReact } from "react-icons/io5";
+import { RiTailwindCssFill } from "react-icons/ri";
+// import { RiJavascriptFill } from "react-icons/ri";
 const Skills = () => {
   return (
     <section className="mx-auto flex w-full flex-col gap-8">
       <h3 className="text-center text-2xl font-semibold">Skill tecniche</h3>
       <div className="flex h-full w-full flex-col flex-wrap gap-8 md:grid md:grid-cols-3">
-        <div className="flex items-center justify-center bg-green-300 p-4">
+        <div className="flex items-center justify-between bg-green-300 p-4">
           JavaScript
+          <IoLogoJavascript color="yellow" size={24} className="bg-black" />
         </div>
         <div className="flex items-center justify-center bg-green-300 p-4">
           tailwind
+          <RiTailwindCssFill size={24} className="fill-cyan-500" />
         </div>
         <div className="flex items-center justify-center bg-green-300 p-4">
           React
+          <IoLogoReact
+            color="blue"
+            size={24}
+            className="animate-spin-slow"
+          />
         </div>
         <div className="flex items-center justify-center bg-green-300 p-4">
           Git
+          <IoLogoJavascript color="yellow" size={24} className="bg-black" />
         </div>
         <div className="flex items-center justify-center bg-green-300 p-4">
           NextUI
+          <IoLogoJavascript color="yellow" size={24} className="bg-black" />
         </div>
         <div className="flex items-center justify-center bg-green-300 p-4">
           NextUI
+          <IoLogoJavascript color="yellow" size={24} className="bg-black" />
         </div>
         <div className="col-span-3 col-start-1">
           <p className="text-center">Guarda i miei lavori</p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,11 @@ export default {
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'spin-slow': 'spin 3s linear infinite',
+      }
+    },
   },
   darkMode: "class",
   plugins: [nextui()],
