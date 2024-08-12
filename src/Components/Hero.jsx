1003d2d4f60dfb6075d61f9e8dfa60dcb3471285
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-12 14:36:02 +0200

    refactor: :art: ristruttura codice
    ristruttura codice in maniera piu semplice ed utilizando flexbox

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,33 +1,35 @@
 import { Button } from "@nextui-org/react";
 
 const Hero = () => {
-  
-
   return (
-    <section className="mx-auto flex h-screen flex-col items-center justify-center gap-4">
-      <div className="space-y-8">
-        <div className="space-y-4">
-          <div className="flex items-center gap-2">
-            {/* icona con animazione */}
-            <div className="relative">
-              <div className="absolute size-4 animate-ping rounded-full bg-green-600/60 duration-1000" />
-              <div className="size-4 rounded-full bg-green-600" />
-            </div>
-
-            <p className="uppercase">disponibile per un&apos;assunzione</p>
+    <section className="w-full">
+      <div className="flex flex-col gap-4 px-8">
+        {/* icona con animazione con testo */}
+        <div className="flex items-center gap-2">
+          <div className="relative">
+            <div className="absolute size-4 animate-ping rounded-full bg-green-600/60 duration-1000" />
+            <div className="size-4 rounded-full bg-green-600" />
           </div>
-          <div>
-            <h2 className="text-4xl">Ciao, sono </h2>
-            <h1 className="text-5xl font-bold">Smailen Vargas</h1>
-          </div>
-          <p className="text-2xl">Frontend developer</p>
+          <p className="uppercase">disponibile per un&apos;assunzione</p>
         </div>
-        <div className="flex flex-col gap-2 md:flex-row">
-          <Button className="flex-1">Contattami</Button>
-          <Button className="flex-1">Progetti</Button>
+
+        {/* presentazione */}
+        <div>
+          <h2 className="text-4xl">Ciao, sono </h2>
+          <h1 className="text-5xl font-bold">Smailen Vargas</h1>
+        </div>
+        <p className="text-2xl">Frontend developer</p>
+
+        {/* call to action */}
+        <div className="md:flex-star flex flex-col flex-wrap gap-2 md:flex-row">
+          <Button className="w-fit" variant="solid">
+            Contattami
+          </Button>
+          <Button className="w-fit" variant="solid">
+            Progetti
+          </Button>
         </div>
       </div>
-      {/* <div className="h-44 w-full bg-red-500"></div> */}
     </section>
   );
 };
