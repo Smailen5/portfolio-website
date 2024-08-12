4d67a06183e7b261ecefb7a6d56790a3cc84d376
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-12 19:18:53 +0200

    refactor: :art: aggiunge navbar
    aggiunge navbar agli screen

--- src/screen/ContactScreen.jsx
+++ src/screen/ContactScreen.jsx
@@ -1,5 +1,14 @@
+import NavbarSample from "../Components/Navbar";
+
 const ContactScreen = () => {
-  return <div>ContactScreen</div>;
+  return (
+    <>
+      <NavbarSample />
+      <div className="min-h-screen bg-red-300 py-16">
+        <div>ContactScreen</div>
+      </div>
+    </>
+  );
 };
 
 export default ContactScreen;
--- src/screen/ProjectsScreen.jsx
+++ src/screen/ProjectsScreen.jsx
@@ -1,5 +1,14 @@
+import NavbarSample from "../Components/Navbar";
+
 const ProjectsScreen = () => {
-  return <div>ProjectsScreen</div>;
+  return (
+    <>
+      <NavbarSample />
+      <div className="min-h-screen bg-green-300 py-16">
+        <div>ProjectsScreen</div>
+      </div>
+    </>
+  );
 };
 
 export default ProjectsScreen;
