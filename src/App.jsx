d3eb730d6ea1ea3751b260b48040d5b785538c60
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-12 14:42:12 +0200

    refactor: :art: crea componente
    crea e componente Presentation per spostarlo fuori da App

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Button } from "@nextui-org/react";
 import Hero from "./Components/Hero";
 import NavbarSample from "./Components/Navbar";
 import FormComponent from "./Components/FormComponent";
+import Presentation from "./Components/Presentation";
 
 const App = () => {
   return (
@@ -12,22 +13,7 @@ const App = () => {
 
         {/* presentazione */}
         <div className="flex flex-col justify-around gap-20">
-          <section className="h-scree mx-auto flex w-3/4 flex-col items-center justify-center gap-4">
-            <p>
-              Ciao, sono Smailen, un frontend developer appassionato di creare
-              interfacce utente intuitive e coinvolgenti.
-            </p>
-            <p>
-              Con competenze in React, Vite e Tailwind CSS, trasformo idee in
-              esperienze digitali fluide e ottimizzate. Sono sempre alla ricerca
-              di nuove tecnologie e puntualmente migliorare le mie competenze
-              per diventare un full-stack developer.
-            </p>
-            <p>
-              Dai un'occhiata ai miei progetti per vedere come posso essere di
-              valore per il tuo team.
-            </p>
-          </section>
+          <Presentation />
 
           {/* sezione competenze */}
           <section className="h-scree mx-auto flex w-3/4 flex-col gap-8">
---
+++ src/Components/Presentation.jsx
@@ -0,0 +1,24 @@
+const Presentation = () => {
+  return (
+    <div className="flex flex-col justify-around gap-20">
+      <section className="h-scree mx-auto flex w-3/4 flex-col items-center justify-center gap-4">
+        <p>
+          Ciao, sono Smailen, un frontend developer appassionato di creare
+          interfacce utente intuitive e coinvolgenti.
+        </p>
+        <p>
+          Con competenze in React, Vite e Tailwind CSS, trasformo idee in
+          esperienze digitali fluide e ottimizzate. Sono sempre alla ricerca di
+          nuove tecnologie e puntualmente migliorare le mie competenze per
+          diventare un full-stack developer.
+        </p>
+        <p>
+          Dai un'occhiata ai miei progetti per vedere come posso essere di
+          valore per il tuo team.
+        </p>
+      </section>
+    </div>
+  );
+};
+
+export default Presentation;
