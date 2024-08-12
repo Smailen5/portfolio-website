fda90ab07ebddaa2581299c67a2e997d2310ed75
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-12 17:20:42 +0200

    refactor: :construction: crea array
    crea array da mandare a Social

---
+++ src/data/social.js
@@ -0,0 +1,14 @@
+[
+  {
+    name: "Github",
+    link: "https://github.com/smailen",
+  },
+  {
+    name: "Linkedin",
+    link: "https://www.linkedin.com/in/smailen/",
+  },
+  {
+    name: "Frontend Mentor",
+    link: "https://www.frontendmentor.io/profile/smailen",
+  },
+];
