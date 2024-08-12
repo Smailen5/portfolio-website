b87d51bff9de8268de4e462ed94cbe8af373baa5
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-12 18:55:16 +0200

    refactor: :fire: commenti
    eleimina codice commentato e importazioni non piu utili

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,3 @@
-// import Hero from "./Components/Hero";
-// import NavbarSample from "./Components/Navbar";
-// import FormComponent from "./Components/FormComponent";
-// import Presentation from "./Components/Presentation";
-// import Skills from "./Components/Skills";
-// import Socials from "./Components/Socials";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/routes";
 
@@ -16,14 +10,3 @@ const App = () => {
 };
 
 export default App;
-
-{/* <NavbarSample />
-      <div className="min-h-screen bg-blue-300 pt-16">
-        <div className="flex flex-col justify-around gap-20 mt-20 px-8">
-          <Hero />
-          <Presentation />
-          <Skills />
-          <Socials />
-          <FormComponent />
-        </div>
-      </div> */}
--- src/screen/HomeScreen.jsx
+++ src/screen/HomeScreen.jsx
@@ -1,6 +1,5 @@
 import Hero from "../Components/Hero";
 import NavbarSample from "../Components/Navbar";
-import FormComponent from "../Components/FormComponent";
 import Presentation from "../Components/Presentation";
 import Skills from "../Components/Skills";
 import Socials from "../Components/Socials";
@@ -9,13 +8,12 @@ const HomeScreen = () => {
   return (
     <>
       <NavbarSample />
-      <div className="min-h-screen bg-blue-300 pt-16">
+      <div className="min-h-screen bg-blue-300 py-16">
         <div className="mt-20 flex flex-col justify-around gap-20 px-8">
           <Hero />
           <Presentation />
           <Skills />
           <Socials />
-          <FormComponent />
         </div>
       </div>
     </>
