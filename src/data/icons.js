3a87c5a62e7af5ed26b5a5228c610b96a2dd403a
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-12 17:51:21 +0200

    refactor: :construction: crea index
    crea l'indice delle icone

---
+++ src/data/icons.js
@@ -0,0 +1,20 @@
+const icons =[
+    {
+        name: "Javascript",
+        icon: '../assets/icons/icons8-javascript-48.svg',
+    },
+    {
+        name: "React",
+        icon: '../assets/icons/icons8-react-48.svg',
+    },
+    {
+        name: "Tailwind",
+        icon: '../assets/icons/icons8-tailwindcss-48.svg',
+    },
+    {
+        name: "Git",
+        icon: '../assets/icons/icons8-git-48.svg',
+    },
+]
+
+export default icons;
