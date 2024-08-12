24d3edfc18157ef843670834fd296325c1a49c2b
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-12 14:53:26 +0200

    refactor: crea componente
    crea componente Skills per spostarlo fuori da App

---
+++ src/Components/Skills.jsx
@@ -0,0 +1,35 @@
+import { Button } from "@nextui-org/react";
+
+const Skills = () => {
+  return (
+    <section className="h-scree mx-auto flex w-3/4 flex-col gap-8">
+      <h3 className="text-center text-2xl font-semibold">Skill tecniche</h3>
+      <div className="justify-cente flex h-full w-full flex-col justify-start gap-8 px-8 md:grid md:grid-cols-3">
+        <div className="flex items-center justify-center bg-green-300 p-4">
+          JavaScript
+        </div>
+        <div className="flex items-center justify-center bg-green-300 p-4">
+          tailwind
+        </div>
+        <div className="flex items-center justify-center bg-green-300 p-4">
+          React
+        </div>
+        <div className="flex items-center justify-center bg-green-300 p-4">
+          Git
+        </div>
+        <div className="flex items-center justify-center bg-green-300 p-4">
+          NextUI
+        </div>
+        <div className="flex items-center justify-center bg-green-300 p-4">
+          NextUI
+        </div>
+        <div className="col-span-3 col-start-1">
+          <p className="text-center">Guarda i miei lavori</p>
+          <Button className="w-full">Portfolio</Button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Skills;
