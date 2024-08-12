060d106d476a43c1245738280cd49567c162dda3
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-12 15:15:38 +0200

    refactor: :lipstick: cambia padding
    sposta il padding x dal contenuto al contenitore

--- src/Components/Socials.jsx
+++ src/Components/Socials.jsx
@@ -1,8 +1,8 @@
 const Socials = () => {
   return (
-    <section className="h-scree mx-auto flex w-3/4 flex-col gap-8">
+    <section className="flex flex-col gap-8 px-8">
       <h3 className="text-center text-2xl font-semibold">Social</h3>
-      <div className="justify-cente flex h-full w-full flex-col justify-start gap-8 px-8 md:grid md:grid-cols-3">
+      <div className="justify-cente flex h-full w-full flex-col justify-start gap-8 md:grid md:grid-cols-3">
         <div className="flex items-center justify-center bg-green-300 p-4">
           Linkedin
         </div>
