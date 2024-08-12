1e36c98a3f57418f4bcf1f344a84cce991391048
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-12 20:37:06 +0200

    refactor: :lipstick: aggiunge campo
    aggiunge nuovo campo al form e da stile, rimuove i placeholder

--- src/Components/FormComponent.jsx
+++ src/Components/FormComponent.jsx
@@ -4,11 +4,15 @@ import { object, string } from "yup";
 import { Button } from "@nextui-org/react";
 
 const validationSchema = object({
-  email: string().required(),
-  message: string().required(),
+  name: string().required("Nome richiesto"),
+  email: string().email("Email non valida").required("Email richiesta"),
+  message: string()
+    .required("Qualcosa devi pur scrivere")
+    .min(100, "Minimo 100 caratteri"),
 });
 
 const initialValues = {
+  name: "",
   email: "",
   message: "",
 };
@@ -28,27 +32,44 @@ const FormComponent = () => {
     >
       {({ errors, touched, handleSubmit, isSubmitting }) => (
         <Form
-          className="flex flex-col items-center gap-4"
+          className="flex flex-col items-start gap-4"
           onSubmit={handleSubmit}
           noValidate
         >
+          <label htmlFor="name">Nome</label>
+          <Field
+            type="name"
+            name="name"
+            className="w-full rounded-md border border-gray-400 p-2 shadow"
+          />
+          <ErrorMessage name="name" component="p" className="text-red-500" />
+
           <label htmlFor="email">Email</label>
-          <Field type="email" name="email" placeholder="Email" />
-          <ErrorMessage name="email" component="p" />
+          <Field
+            type="email"
+            name="email"
+            className="w-full rounded-md border border-gray-400 p-2 shadow"
+          />
+          <ErrorMessage name="email" component="p" className="text-red-500" />
 
           <label htmlFor="message">Message</label>
           <Field
             as="textarea"
             type="text"
             name="message"
-            placeholder="Message"
             render={({ field, form: { touched, errors } }) => (
-              <textarea {...field} placeholder="message" cols={30} rows={10} />
+              <textarea
+                {...field}
+                rows={10}
+                className="w-full resize-none rounded-md border border-gray-400 p-2 shadow"
+              />
             )}
           />
-          <ErrorMessage name="message" component="p" />
+          <ErrorMessage name="message" component="p" className="text-red-500" />
 
-          <Button type="submit">Submit</Button>
+          <Button type="submit" color="primary" className="w-full">
+            Submit
+          </Button>
         </Form>
       )}
     </Formik>
