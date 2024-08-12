3a6921efb5fc968db764692cce2ebba63aae1c9e
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-12 19:41:42 +0200

    refactor: :lipstick: aggiunge ombra
    aggiunge sfumatura e arrotonda bordi

--- src/Components/Social.jsx
+++ src/Components/Social.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 const Social = ({ name, link }) => {
   return (
-    <div className="flex items-center justify-center bg-blue-600 p-4">
+    <div className="flex items-center justify-center rounded-md bg-blue-600 p-4 shadow-md shadow-blue-500">
       <a href={link} className="font-semibold uppercase text-white">
         {name}
       </a>
