2ab30b33d299718d6e294302920edd1cfe4b5187
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-12 15:05:06 +0200

    refactor: :art: sposta componente
    sposta la sezione competenze da App al componente Skills e importa il componente Skills in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { Button } from "@nextui-org/react";
 import Hero from "./Components/Hero";
 import NavbarSample from "./Components/Navbar";
 import FormComponent from "./Components/FormComponent";
 import Presentation from "./Components/Presentation";
+import Skills from "./Components/Skills";
 
 const App = () => {
   return (
@@ -15,35 +15,7 @@ const App = () => {
           <Presentation />
 
           {/* sezione competenze */}
-          <section className="h-scree mx-auto flex w-3/4 flex-col gap-8">
-            <h3 className="text-center text-2xl font-semibold">
-              Skill tecniche
-            </h3>
-            <div className="justify-cente flex h-full w-full flex-col justify-start gap-8 px-8 md:grid md:grid-cols-3">
-              <div className="flex items-center justify-center bg-green-300 p-4">
-                JavaScript
-              </div>
-              <div className="flex items-center justify-center bg-green-300 p-4">
-                tailwind
-              </div>
-              <div className="flex items-center justify-center bg-green-300 p-4">
-                React
-              </div>
-              <div className="flex items-center justify-center bg-green-300 p-4">
-                Git
-              </div>
-              <div className="flex items-center justify-center bg-green-300 p-4">
-                NextUI
-              </div>
-              <div className="flex items-center justify-center bg-green-300 p-4">
-                NextUI
-              </div>
-              <div className="col-span-3 col-start-1">
-                <p className="text-center">Guarda i miei lavori</p>
-                <Button className="w-full">Portfolio</Button>
-              </div>
-            </div>
-          </section>
+          <Skills />
 
           {/* sezione social */}
           <section className="h-scree mx-auto flex w-3/4 flex-col gap-8">
