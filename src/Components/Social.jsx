75d58817c0c613ce329b8f2c0885c05354c42671
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-12 17:19:25 +0200

    refactor: :construction: crea componente
    crea il componente Social per gestire i link social

---
+++ src/Components/Social.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/prop-types */
+const Social = ({ children }) => {
+  return (
+    <div className="flex items-center justify-center bg-green-300 p-4">
+      {children}
+    </div>
+  );
+};
+
+export default Social;
