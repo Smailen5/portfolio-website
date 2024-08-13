bf45f77532c6da7ea10b80814cd6e01048caad90
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-13 14:06:00 +0200

    refactor: :lipstick: rimuove background
    rimuove colore di background

--- src/screen/HomeScreen.jsx
+++ src/screen/HomeScreen.jsx
@@ -8,7 +8,7 @@ const HomeScreen = () => {
   return (
     <>
       <NavbarSample />
-      <div className="xl:flex min-h-screen xl:justify-center bg-blue-300 py-16">
+      <div className="xl:flex min-h-screen xl:justify-center py-16">
         <div className="mt-20 flex flex-col justify-around gap-20 px-6 xl:max-w-[1024px]">
           <Hero />
           <Presentation />
