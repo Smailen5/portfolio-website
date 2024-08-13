1c7464d2e92d3a7efe1e550332213f077bb649b0
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-13 12:55:36 +0200

    refactor: :construction: crea indici
    crea struttura indici immagini e json

---
+++ src/data/images.js
@@ -0,0 +1,20 @@
+import thumbnailProject1 from "../assets/images/base-apparel-desktop.jpeg";
+import thumbnailProject2 from "../assets/images/desktop password generator.jpeg";
+import thumbnailProject3 from "../assets/images/desktop tip calculator.jpeg";
+
+const images = {
+    thumbnailProject1: {
+        small: thumbnailProject1,
+        large: thumbnailProject1,
+    },
+    thumbnailProject2: {
+        small: thumbnailProject2,
+        large: thumbnailProject2,
+    },
+    thumbnailProject3: {
+        small: thumbnailProject3,
+        large: thumbnailProject3,
+    },
+}
+
+export default images
