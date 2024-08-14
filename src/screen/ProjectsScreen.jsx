e2a69907ae3659d4d1723cb2b06fd29ba7ac3bac
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-14 15:20:49 +0200

    refactor: :speech_balloon: aggiorna descrizione
    aggiorna la descrizione del progetto con una bozza di testo

--- src/screen/ProjectsScreen.jsx
+++ src/screen/ProjectsScreen.jsx
@@ -8,7 +8,19 @@ const ProjectsScreen = () => {
       <NavbarSample />
       <div className="flex min-h-screen justify-center bg-neutral-50 py-16">
         <div className="mt-20 flex flex-col justify-around gap-20 px-6 text-center xl:max-w-[1024px]">
-          <h4>Breve introduzione di tutti i progetti che ho sviluppato</h4>
+          <p>
+            Questi progetti sono stati realizzati come esercizi su Frontend
+            Mentor, con l'obiettivo di affinare le competenze che ho acquisito
+            durante i miei studi. Sono esercizi che mi hanno permesso di mettere
+            in pratica e migliorare le tecnologie apprese, affrontando sfide
+            reali di sviluppo front-end.
+          </p>
+          <p>
+            Se siete interessati a vedere tutti i miei lavori su Frontend Mentor
+            e React, potete visitare il file README. Inoltre, su GitHub
+            troverete una raccolta completa di tutti i miei progetti, inclusi
+            quelli reali che sono attualmente in uso.
+          </p>
           {/* CONTENITORE PROGETTI */}
           <section className="grid gap-4 lg:grid-cols-2">
             {projects.map((project) => {
