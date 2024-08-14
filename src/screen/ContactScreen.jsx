dc021e36ddf4b1c3782a4a7454498a0c5495d4ca
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-14 15:53:44 +0200

    refactor: :speech_balloon: aggiorna testo
    aggiorna il testo con il definitivo

--- src/screen/ContactScreen.jsx
+++ src/screen/ContactScreen.jsx
@@ -5,14 +5,17 @@ const ContactScreen = () => {
   return (
     <>
       <NavbarSample />
-      <div className="flex min-h-screen justify-center bg-red-300 py-16">
+      <div className="flex min-h-screen justify-center bg-neutral-50 py-16">
         <div className="mt-20 flex flex-col justify-around gap-20 px-6 text-center xl:max-w-[1024px]">
-          <h3>Contattami</h3>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-            Perferendis fuga molestias a dolorum eos, vitae laudantium nulla
-            eligendi, hic necessitatibus sapiente.
-          </p>
+          <div className="space-y-4">
+            <h3 className="text-2xl font-semibold">Contattami</h3>
+            <p>
+              Se hai domande, richieste di collaborazione, o semplicemente vuoi
+              parlare di React, non esitare a usare il modulo qui sotto per
+              metterti in contatto con me. Sarò felice di risponderti il prima
+              possibile!
+            </p>
+          </div>
           <FormComponent />
         </div>
       </div>
