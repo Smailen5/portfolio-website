1bc4b80cc4a8c75c28c6f3a011580fd8a0fda850
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-14 19:14:16 +0200

    refactor: :lipstick: cambia intervallo
    cambia velocita animazione intervenendo su su setTimeout e setInterval

--- src/utils/useChangeAvatar.js
+++ src/utils/useChangeAvatar.js
@@ -19,8 +19,8 @@ const useChangeAvatar = () => {
         const randomIndex = Math.floor(Math.random() * filteredImages.length);
         setCurrentAvatar(filteredImages[randomIndex]);
         setAnimation("animate-appearance-in");
-      }, 300);
-    }, 2000);
+      }, 50);
+    }, 5000);
     return () => clearInterval(interval);
   }, [currentAvatar]);
   return { currentAvatar, animation };
