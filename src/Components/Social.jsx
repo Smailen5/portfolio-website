026fd4f9e77107a7eb91147fae28fb95d8ce62db
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-14 17:56:58 +0200

    refactor: :closed_lock_with_key: aggiunge attributi
    aggiunge attributi target e rel per i link social

--- src/Components/Social.jsx
+++ src/Components/Social.jsx
@@ -2,7 +2,7 @@
 const Social = ({ name, link }) => {
   return (
     <div className="flex items-center justify-center rounded-md bg-blue-600 p-4 shadow-md shadow-blue-500">
-      <a href={link} className="font-semibold uppercase text-white">
+      <a href={link} target="_blank" rel="noopener noreferrer" className="font-semibold uppercase text-white">
         {name}
       </a>
     </div>
