a1c23cff5a4cee130acf19a8f5a8c00dddd54e11
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-14 17:55:15 +0200

    refactor: :art: aggiunge link
    aggiunge link al bottone che rimanda ai progetti

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -4,11 +4,14 @@ import { SiNextui } from "react-icons/si";
 import skillsData from "../data/skillsData";
 import Skill from "./Skill";
 import { GoProjectRoadmap } from "react-icons/go";
+import { Link } from "react-router-dom";
 // import { RiJavascriptFill } from "react-skills/ri";
 const Skills = () => {
   return (
     <section className="mx-auto flex w-full flex-col gap-8">
-      <h3 className="text-center text-2xl font-semibold">Competenze acquisite</h3>
+      <h3 className="text-center text-2xl font-semibold">
+        Competenze acquisite
+      </h3>
       <div className="flex h-full w-full flex-col flex-wrap gap-8 md:grid md:grid-cols-3">
         {skillsData.map((skill) => {
           return <Skill key={skill.name} {...skill} />;
@@ -21,8 +24,14 @@ const Skills = () => {
 
         <div className="space-y-4 md:col-start-2">
           <p className="text-center">Guarda i miei lavori</p>
-          <Button variant="shadow" color="secondary" size="lg" endContent={<GoProjectRoadmap />} className="w-full">
-            Portfolio
+          <Button
+            variant="shadow"
+            color="secondary"
+            size="lg"
+            endContent={<GoProjectRoadmap />}
+            className="w-full"
+          >
+            <Link to={"/projects"}>Portfolio</Link>
           </Button>
         </div>
       </div>
