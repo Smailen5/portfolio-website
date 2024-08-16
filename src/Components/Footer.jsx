e1bbe107d08be238ac25168d7817a169d4e9bd40
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-16 17:28:48 +0200

    refactor: :iphone: aggiunge classi
    rende responsive il footer per tutti gli schermi

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -6,20 +6,25 @@ import { SiNextui } from "react-icons/si";
 
 const Footer = () => {
   return (
-    <div className="bg-red-30 w-full bg-white p-4 text-center">
-      <div className="flex justify-center gap-4">
-        <img src={tailwindIcon} alt="icona tailwind css" className="size-6" />
-        <img src={reactIcon} alt="icona react" className="size-6" />
-        <SiNextui alt="icona nextui" className="size-6" />
+    <footer className="bg-red-30 w-full bg-white p-4 px-6 text-center xl:flex xl:justify-center">
+      <div className="flex max-w-[1024px] flex-col gap-2 md:flex-row md:items-center md:justify-between xl:min-w-[1024px]">
+        <div className="flex justify-center gap-4 md:order-2">
+          <p className="hidden md:block">Powered by</p>
+          <img src={tailwindIcon} alt="icona tailwind css" className="size-6" />
+          <img src={reactIcon} alt="icona react" className="size-6" />
+          <SiNextui alt="icona nextui" className="size-6" />
+        </div>
+        <p>
+          © 2024 Smailen Vargas. Tutti i diritti riservati.{" "}
+          <span>
+            Icons by{" "}
+            <a href="https://icons8.com" className="text-blue-400 underline">
+              Icons8
+            </a>
+          </span>
+        </p>
       </div>
-      <p>© 2024 Smailen Vargas. Tutti i diritti riservati.</p>
-      <p>
-        Icons by{" "}
-        <a href="https://icons8.com" className="text-blue-400 underline">
-          Icons8
-        </a>
-      </p>
-    </div>
+    </footer>
   );
 };
 
