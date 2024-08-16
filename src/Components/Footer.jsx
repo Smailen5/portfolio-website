f70e9ec439b1d8e561606ece3cb7ab614487dd94
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-16 14:49:11 +0200

    refactor: :lipstick: crea componente
    crea il componente footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,5 +1,20 @@
+/* eslint-disable no-unused-vars */
+// import skillsData from "../data/skillsData";
+import tailwindIcon from "../assets/icons/icons8-tailwind-css.svg";
+import reactIcon from "../assets/icons/icons8-react.svg";
+import { SiNextui } from "react-icons/si";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="w-full bg-red-30 p-4 text-center bg-white">
+      <div className="flex justify-center gap-4">
+        <img src={tailwindIcon} alt="icona tailwind css" className="size-6" />
+        <img src={reactIcon} alt="icona react" className="size-6" />
+        <SiNextui alt="icona nextui" className="size-6" />
+      </div>
+      <p>© 2024 Smailen Vargas. Tutti i diritti riservati.</p>
+    </div>
+  );
 };
 
 export default Footer;
