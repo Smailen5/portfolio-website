663482735f8e0b9dcc0912f3620a8af407d987d4
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-16 20:42:37 +0200

    refactor: :construction: aggiunge scrollPosition
    crea scrollPosition per controlla lo scroll e ritornare al valore corretto

--- src/utils/context.jsx
+++ src/utils/context.jsx
@@ -1,22 +1,35 @@
 /* eslint-disable react-refresh/only-export-components */
 import { createContext, useContext } from "react";
 import PropTypes from "prop-types";
+import { useState } from "react";
 
 const AppContext = createContext();
 
 const AppProvider = ({ children }) => {
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  const getScrollPosition = (value) => {
+    setScrollPosition(value);
+  };
+
+  const deleteScrollPosition = () => {
+    setScrollPosition(0);
+  };
+
   return (
-    <AppContext.Provider value="contenuto da context">
+    <AppContext.Provider
+      value={{ scrollPosition, getScrollPosition, deleteScrollPosition }}
+    >
       {children}
     </AppContext.Provider>
   );
 };
 
-const useGlobalContext = ()=>{
-    return useContext(AppContext)
-}
+const useGlobalContext = () => {
+  return useContext(AppContext);
+};
 
 AppProvider.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 export { AppProvider, useGlobalContext };
