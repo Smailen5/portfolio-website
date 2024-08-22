428d5fb0fb9dd372690236f7a45be62c9a99e61d
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-08-22 14:08:07 +0200

    refactor: :art: modifica componente
    rimuove div in favore di Button nel componente

--- src/Components/Social.jsx
+++ src/Components/Social.jsx
@@ -1,8 +1,12 @@
+import { Button } from "@nextui-org/react";
 import PropTypes from "prop-types";
 
 const Social = ({ name, link }) => {
   return (
-    <div className="flex items-center justify-center rounded-md bg-blue-600 p-4 shadow-md shadow-blue-500">
+    <Button
+      size="lg"
+      className="flex items-center justify-center rounded-md bg-blue-600 p-4 shadow-md shadow-blue-500"
+    >
       <a
         href={link}
         target="_blank"
@@ -11,13 +15,13 @@ const Social = ({ name, link }) => {
       >
         {name}
       </a>
-    </div>
+    </Button>
   );
 };
 
 Social.propTypes = {
   name: PropTypes.string.isRequired,
   link: PropTypes.string.isRequired,
-}
+};
 
 export default Social;
