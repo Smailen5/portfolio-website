9693edbaded6202d07f5a6b9d0fed72ac4e2c350
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-10-09 15:44:05 +0200

    fix: :truck: rimuovi estensione
    rimuovo estensione nel link che rimanda al file jsx e non a tsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { AppProvider } from "./utils/context.jsx";
+import { AppProvider } from "./utils/context";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
