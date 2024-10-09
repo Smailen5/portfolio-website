7dd02fbd0f1595a9cbf1d830e411c1cbe794e827
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-10-09 15:55:27 +0200

    fix: :truck: rinomina estensioni
    rinomino estensione da js a ts per skillsData e social, aggiorno i link con esensione corretta in main

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { NextUIProvider } from "@nextui-org/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.js";
+import App from "./App.tsx";
 import "./index.css";
-import { AppProvider } from "./utils/context.js";
+import { AppProvider } from "./utils/context.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
