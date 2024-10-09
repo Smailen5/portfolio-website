9ffc2b472cc1cb60750329f60d20e34091df3316
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-10-09 15:46:31 +0200

    fix: :truck: rinomina estensioni
    rinomino estensioni da jsx a tsx per App e main, aggiorno il link che rimanda a main in index.html

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { NextUIProvider } from "@nextui-org/react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import App from "./App.js";
 import "./index.css";
-import { AppProvider } from "./utils/context";
+import { AppProvider } from "./utils/context.js";
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AppProvider>
       <NextUIProvider>
