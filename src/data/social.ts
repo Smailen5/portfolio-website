47fd0a60ada227a0610d5f0d1e8043f8f72143ed
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-10-10 15:05:37 +0200

    refactor: :card_file_box: aggiungo chiave
    aggiungo chiave per le icone dei link

--- src/data/social.ts
+++ src/data/social.ts
@@ -2,15 +2,19 @@ const social = [
   {
     name: "Github",
     link: "https://github.com/Smailen5",
+    icon: "github",
   },
   {
     name: "Linkedin",
     link: "https://www.linkedin.com/in/smailen-vargas/",
+    icon: "linkedin",
   },
   {
     name: "Frontend Mentor",
     link: "https://www.frontendmentor.io/profile/Smailen5",
+    icon: "frontend-mentor",
   },
 ];
 
 export default social;
+
