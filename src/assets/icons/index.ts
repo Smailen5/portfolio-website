19cc431fb026796cbb5c11a7ce3033cc148b5660
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-10-10 15:04:09 +0200

    feat: :art: aggiungi icone
    aggiungo icone svg e creo indice

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,9 @@
+import GitHubLogoIcon from "./GitHubIcon.svg";
+import LinkedInLogoIcon  from "./LinkedInLogoIcon.svg";
+import { SiFrontendmentor } from "react-icons/si";
+
+export const icons = {
+  github: GitHubLogoIcon,
+  linkedin: LinkedInLogoIcon,
+  "frontend-mentor": SiFrontendmentor,
+};
