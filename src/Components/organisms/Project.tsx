afef5c7e83d6fd7b18f514dab7f47c3eb858d0b4
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-10-10 12:53:44 +0200

    refactor: :fire: rimuovi codice
    rimuovo codice commentato e verifica imageKey

--- src/Components/organisms/Project.tsx
+++ src/Components/organisms/Project.tsx
@@ -1,4 +1,3 @@
-import { images } from "../../data/images";
 import { ButtonsProject } from "../molecules/ButtonsProject";
 import { ContainerProject } from "../molecules/ContainerProject";
 import { PictureProject } from "../molecules/PictureProject";
@@ -18,30 +17,12 @@ const Project: React.FC<ProjectType> = ({
   liveSite,
   github,
 }) => {
-  const image = images[imageKey];
-
-  if (!image) {
-    console.error(`La chiave ${imageKey} non e stata trovata`);
-    return null;
-  }
-
   return (
     <>
       {/* PROGETTO SINGOLO */}
       <div className="flex flex-col gap-4 rounded-md bg-white shadow-md">
-      <PictureProject linkSite={liveSite} imageKey={imageKey} />
         {/* immagine */}
-        {/* <picture>
-          <source media="(1024px)" srcSet={images[imageKey].large} />
-          <a href={liveSite} target="_blank" rel="noopener noreferrer">
-            <img
-              loading="lazy"
-              src={images[imageKey].small}
-              // forse conviene dare una misura fissa a tutte le immagini per evitare dimensioni diverse es.`lg:w-[480px] lg:h-[300px]`
-              className="rounded-md shadow-lg lg:hover:animate-pulse"
-            />
-          </a>
-        </picture> */}
+        <PictureProject linkSite={liveSite} imageKey={imageKey} />
         {/* contenitore nome progetto e descrizione */}
         <ContainerProject title={title} technologies={technologies}>
           <ButtonsProject linkSite={liveSite} linkRepo={github} />
