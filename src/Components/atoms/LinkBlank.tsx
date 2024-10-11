05ffcb60e31c3c67ad7bcbbd0cc5eea343acaa20
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-10-11 13:17:47 +0200

    feat: :sparkles: aggiungi parametro
    aggiungo isText per gestire i link testuali, ora posso usare il componente come semplice link e link testuale

--- src/Components/atoms/LinkBlank.tsx
+++ src/Components/atoms/LinkBlank.tsx
@@ -1,15 +1,16 @@
 type LinkBlank = {
   href: string;
   children: React.ReactNode;
+  isText?: boolean;
 };
 
-export const LinkBlank: React.FC<LinkBlank> = ({ href, children }) => {
+export const LinkBlank: React.FC<LinkBlank> = ({ href, children, isText }) => {
   return (
     <a
       href={href}
       target="_blank"
       rel="noopener noreferrer"
-      className="text-blue-500 underline"
+      className={`${isText && "text-blue-500 underline"}`}
     >
       {children}
     </a>
