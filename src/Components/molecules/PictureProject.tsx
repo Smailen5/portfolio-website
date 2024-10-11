1a7451206cb9ae23f4e6a2f33ba6d309211ecda5
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-10-11 13:19:30 +0200

    refactor: :fire: rimuove codice
    rimuovo codice commentato del vecchio link

--- src/Components/molecules/PictureProject.tsx
+++ src/Components/molecules/PictureProject.tsx
@@ -21,14 +21,12 @@ export const PictureProject: React.FC<PictureProject> = ({
     <picture className="overflow-hidden rounded-md">
       <source media="(1024px)" srcSet={image.large} />
       <LinkBlank href={linkSite}>
-        {/* <a href={linkSite} target="_blank" rel="noopener noreferrer"> */}
         <img
           loading="lazy"
           src={image.small}
           // forse conviene dare una misura fissa a tutte le immagini per evitare dimensioni diverse es.`lg:w-[480px] lg:h-[300px]`
           className="shadow-lg transition-all duration-500 lg:hover:scale-110"
         />
-        {/* </a> */}
       </LinkBlank>
     </picture>
   );
