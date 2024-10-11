dd73da962a6dbbac411e5524a0fd2e1ccba46282
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-10-11 12:46:24 +0200

    refactor: :art: utilizza template
    utilizzo il template per la pagina progetti

--- src/pages/ProjectPage.tsx
+++ src/pages/ProjectPage.tsx
@@ -1,28 +1,14 @@
-import { HeaderProject } from "@/Components/molecules/HeaderProject";
-import Footer from "../Components/organisms/Footer";
-import NavbarSample from "../Components/organisms/Navbar";
-import Project from "../Components/organisms/Project";
-import projects from "../data/projects.json";
-import useScroll from "../utils/useScroll";
+// import useScroll from "../utils/useScroll";
+import { Layout } from "@/Components/organisms/Layout";
+import { ProjectTemplate } from "@/Components/template/ProjectTemplate";
 
 const ProjectsScreen = () => {
-  useScroll();
+  // useScroll();
   return (
     <>
-      <NavbarSample />
-      <div className="flex min-h-screen justify-center bg-neutral-50 py-16">
-        <div className="mt-20 flex flex-col justify-around gap-20 px-6 text-center xl:max-w-[1024px]">
-          <HeaderProject />
-          
-          {/* CONTENITORE PROGETTI */}
-          <section className="grid gap-4 lg:grid-cols-2">
-            {projects.map((project) => {
-              return <Project key={project.id} {...project} />;
-            })}
-          </section>
-        </div>
-      </div>
-      <Footer />
+      <Layout>
+        <ProjectTemplate />
+      </Layout>
     </>
   );
 };
