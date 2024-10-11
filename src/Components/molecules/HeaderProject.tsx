9bfdec8f6cd4936b01ffdc478de5da0ca1b793f3
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-10-11 12:31:47 +0200

    refactor: :lipstick: modifica allineamento testo
    allineao il testo a sinistra e aggiorno il testo dei paragrafi

--- src/Components/molecules/HeaderProject.tsx
+++ src/Components/molecules/HeaderProject.tsx
@@ -3,22 +3,22 @@ import { LinkBlank } from "../atoms/LinkBlank";
 export const HeaderProject = () => {
   return (
     <>
-      <div className="space-y-4">
+      <div className="space-y-4 text-left">
         <p>
           Questi progetti sono esercizi di Frontend Mentor, creati per affinare
           le competenze acquisite durante i miei studi, affrontando sfide di
-          sviluppo front-end.
+          accessibilità e design.
         </p>
         <p>
-          Puoi vedere tutti i miei lavori migliori su{" "}
+          Puoi vedere il mio portfolio su{" "}
           <LinkBlank href="https://smailen5.github.io/Frontend-Mentor-Challenge/">
             Frontend Mentor
           </LinkBlank>
-          . Su{" "}
+          . Mentre su{" "}
           <LinkBlank href="https://github.com/Smailen5?tab=repositories">
             GitHub
           </LinkBlank>{" "}
-          trovi anche una raccolta completa, inclusi i progetti reali in uso.
+          trovi la raccolta completa, inclusi i progetti reali in uso.
         </p>
       </div>
     </>
