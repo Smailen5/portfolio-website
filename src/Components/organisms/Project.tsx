469c5d6218c94b0290c7af3710ee9b80185f5677
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-10-15 13:36:20 +0200

    refactor: :building_construction: riutilizza componente
    riutilizzo componente passando la prop delle immagini corrette

--- src/Components/organisms/Project.tsx
+++ src/Components/organisms/Project.tsx
@@ -1,20 +1,22 @@
+
 import { ButtonsProject } from "../molecules/ButtonsProject";
 import { ContainerProject } from "../molecules/ContainerProject";
-// import { PictureProject } from "../molecules/PictureProject";
+import { PictureProject } from "../molecules/PictureProject";
 
 type ProjectType = {
   name: string;
   path: string;
   html_url: string;
+  image:string;
 };
 
-const Project: React.FC<ProjectType> = ({ name, path, html_url }) => {
+const Project: React.FC<ProjectType> = ({ name, path, html_url, image }) => {
   return (
     <>
       {/* PROGETTO SINGOLO */}
       <div className="flex flex-col gap-4 rounded-md bg-white shadow-md">
         {/* immagine */}
-        {/* <PictureProject linkSite={liveSite} imageKey={imageKey} /> */}
+        <PictureProject linkSite={path} image={image} />
         {/* contenitore nome progetto e descrizione */}
         <ContainerProject name={name}>
           <ButtonsProject linkSite={path} linkRepo={html_url} />
