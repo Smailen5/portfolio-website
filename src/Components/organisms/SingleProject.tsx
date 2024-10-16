49f7d8af193dc502150e09cafe152832b9df07f7
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-10-16 16:13:31 +0200

    feat: :triangular_flag_on_post: mostra file
    creo componente per mostrare i file readme

---
+++ src/Components/organisms/SingleProject.tsx
@@ -0,0 +1,25 @@
+import { useFetchReadme } from "@/utils/useFetchReadme";
+import { useParams } from "react-router-dom";
+import { Layout } from "./Layout";
+import { MarkdownRenderer } from "../atoms/MarkdownRenderer";
+
+export const SingleProject = () => {
+  const { nome } = useParams<{ nome: string }>(); // Recupera il parametro "nome" dal percorso
+
+  const { readmeContent, error, loading } = useFetchReadme(nome || "");
+
+  return (
+    <Layout>
+      <h1 className="mb- text-2xl font-bold">{nome}</h1>
+      <div className="readme-content">
+        {loading ? (
+          <p>Caricamento...</p>
+        ) : error ? (
+          <p>{error}</p>
+        ) : (
+          <MarkdownRenderer content={readmeContent} />
+        )}
+      </div>
+    </Layout>
+  );
+};
