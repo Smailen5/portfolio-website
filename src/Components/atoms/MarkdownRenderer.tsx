b32278063ee72e03b9f62f890f48337aeda9632d
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-10-16 17:23:31 +0200

    style: :art: identa codice

--- src/Components/atoms/MarkdownRenderer.tsx
+++ src/Components/atoms/MarkdownRenderer.tsx
@@ -17,21 +17,36 @@ export const MarkdownRenderer: React.FC<MarkdownRendererProps> = ({
         remarkPlugins={[remarkGfm]}
         rehypePlugins={[rehypeRaw]}
         components={{
-            h1: ({ node, ...props }) => <h1 className="text-3xl font-bold my-4" {...props} />,
-            h2: ({ node, ...props }) => <h2 className="text-2xl font-semibold my-3" {...props} />,
-            h3: ({ node, ...props }) => <h3 className="text-xl font-semibold my-2" {...props} />,
-            p: ({ node, ...props }) => <p className="my-2" {...props} />,
-            a: ({ node, ...props }) => (
-              <LinkBlank isText href={props.href} {...props} >
-                {props.children}
-              </LinkBlank>
-            ),
-            pre: ({node, ...props}) => <pre className="bg-gray-300 overflow-auto p-2 my-4" {...props} />,
-            img: ({node, ...props}) => <img src={`${import.meta.env.VITE_GITHUB_URL_IMAGES_SCREEN}/${props.src}`} />}}
+          h1: ({ node, ...props }) => (
+            <h1 className="my-4 text-3xl font-bold" {...props} />
+          ),
+          h2: ({ node, ...props }) => (
+            <h2 className="my-3 text-2xl font-semibold" {...props} />
+          ),
+          h3: ({ node, ...props }) => (
+            <h3 className="my-2 text-xl font-semibold" {...props} />
+          ),
+          p: ({ node, ...props }) => <p className="my-2" {...props} />,
+          a: ({ node, ...props }) => (
+            <LinkBlank isText href={props.href} {...props}>
+              {props.children}
+            </LinkBlank>
+          ),
+          pre: ({ node, ...props }) => (
+            <pre
+              className="my-4 overflow-auto rounded-lg bg-gray-200 p-4"
+              {...props}
+            />
+          ),
+          img: ({ node, ...props }) => (
+            <img
+              src={`${import.meta.env.VITE_GITHUB_URL_IMAGES_SCREEN}/${props.src}`}
+            />
+          ),
+        }}
       >
         {content}
       </ReactMarkdown>
     </div>
   );
 };
-
