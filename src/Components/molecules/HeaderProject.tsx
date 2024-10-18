a99b7cae747d5872477346ed0f05b9de0bf22f73
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-10-18 12:24:04 +0200

    refactor: :speech_balloon: aggiorna header
    aggiorno header della sezione progetti

--- src/Components/molecules/HeaderProject.tsx
+++ src/Components/molecules/HeaderProject.tsx
@@ -5,24 +5,32 @@ export const HeaderProject = () => {
     <>
       <div className="space-y-4 text-left">
         <p>
-          Questi progetti sono esercizi di Frontend Mentor, creati per affinare
-          le competenze acquisite durante i miei studi, affrontando sfide di
-          accessibilità e design.
+          Questi progetti sono stati sviluppati nell'ambito delle sfide di
+          Frontend Mentor e sono stati fondamentali per affinare le competenze
+          acquisite durante i miei studi. Alcuni sono stati trattati come veri e
+          propri progetti reali, completamente funzionanti e con particolare
+          attenzione a accessibilità e design.
         </p>
         <p>
-          Puoi vedere il mio portfolio su{" "}
+          Alcuni dei progetti più vecchi, risalenti all'anno scorso, potrebbero
+          contenere errori o soluzioni meno ottimali. Ho deciso di lasciarli
+          intatti per mostrare il mio percorso di miglioramento e la crescita
+          come sviluppatore. In futuro, mi piacerebbe correggerli e ottimizzarli
+          per fare esperienza nel migliorare codice "vecchio".
+        </p>
+        <p>
+          Vuoi scoprire di più? Visita il mio{" "}
           <LinkBlank
-            href="https://smailen5.github.io/Frontend-Mentor-Challenge/" isText
+            href="https://smailen5.github.io/Frontend-Mentor-Challenge/"
+            isText
           >
-            Frontend Mentor
-          </LinkBlank>
-          . Mentre su{" "}
-          <LinkBlank
-            href="https://github.com/Smailen5?tab=repositories" isText
-          >
-            GitHub
+            profilo Frontend Mentor
           </LinkBlank>{" "}
-          trovi la raccolta completa, inclusi i progetti reali in uso.
+          per vedere le sfide completate o il mio{" "}
+          <LinkBlank href="https://github.com/Smailen5?tab=repositories" isText>
+            profilo GitHub
+          </LinkBlank>{" "}
+          per tutti i progetti, incluso il codice sorgente di questo sito.
         </p>
       </div>
     </>
