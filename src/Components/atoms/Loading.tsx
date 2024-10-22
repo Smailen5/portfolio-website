9d09e79a9395317b17d40bcb8b0ec4d32282d75e
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-10-22 14:32:23 +0200

    feat: :lipstick: crea componente
    creo il componente Loading

---
+++ src/Components/atoms/Loading.tsx
@@ -0,0 +1,15 @@
+import spinner from "../../assets/icons/svg-spinners--6-dots-rotate.svg";
+
+export const Loading = () => {
+  return (
+    <section
+      aria-labelledby="loading"
+      className="flex items-center justify-center"
+    >
+      <p id="loading" className="sr-only">
+        Caricamento in corso, attendi
+      </p>
+      <img src={spinner} alt="spinner icon" className="size-10" />
+    </section>
+  );
+};
