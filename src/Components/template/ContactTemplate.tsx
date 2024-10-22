53524371f7014fce2399d71626ee189629a946e0
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-10-22 13:26:01 +0200

    refactor: :fire: rimuovi codice commentato

--- src/Components/template/ContactTemplate.tsx
+++ src/Components/template/ContactTemplate.tsx
@@ -4,12 +4,10 @@ import FormComponent from "../organisms/FormComponent";
 export const ContactTemplate = () => {
   return (
     <>
-      {/* <main className="flex min-h-screen justify-center py-16"> */}
-        <div className="flex flex-col justify-around gap-20 text-center md:max-w-[1024px] lg:flex-row lg:items-center lg:gap-10 md:flex-row">
-          <HeroContact />
-          <FormComponent />
-        </div>
-      {/* </main> */}
+      <div className="flex flex-col justify-around gap-20 text-center md:max-w-[1024px] md:flex-row lg:flex-row lg:items-center lg:gap-10">
+        <HeroContact />
+        <FormComponent />
+      </div>
     </>
   );
 };
