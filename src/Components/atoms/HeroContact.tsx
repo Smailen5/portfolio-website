667039b756e7acdeeff8ca5576ed902228046cc4
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-10-22 12:54:44 +0200

    refactor: :wheelchair: migliora accessibilita
    ho sostituito il primo div con section e cambiato h3 in h1, aggiungo anche l'attributo aria-labelledby e id per gli screen reader

--- src/Components/atoms/HeroContact.tsx
+++ src/Components/atoms/HeroContact.tsx
@@ -1,12 +1,12 @@
 export const HeroContact = () => {
   return (
-    <div className="space-y-4 lg:w-1/2 lg:text-left">
-      <h3 className="text-2xl font-semibold">Contattami</h3>
+    <section aria-labelledby="contact-heading" className="space-y-4 lg:w-1/2 lg:text-left">
+      <h1 id="contact-heading" className="text-3xl font-semibold">Contattami</h1>
       <p>
         Se hai domande, richieste di collaborazione, o semplicemente vuoi
         parlare di React, non esitare a usare il modulo qui sotto per metterti
         in contatto con me. Sarò felice di risponderti il prima possibile!
       </p>
-    </div>
+    </section>
   );
 };
