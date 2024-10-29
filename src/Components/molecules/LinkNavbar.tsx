1e66d91ad45c60a88985c35f666b9431233a143b
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-10-29 16:35:55 +0100

    refactor: :recycle: modifica componente
    creo nuovo componente per gestire lo stile e i link in un solo posto

--- src/Components/molecules/LinkNavbar.tsx
+++ src/Components/molecules/LinkNavbar.tsx
@@ -1,16 +1,19 @@
-import { Link } from "react-router-dom";
+import { LinkComponent } from "../atoms/LinkComponent";
 
 export const LinkNavbar = ({ linkCurriculum }: { linkCurriculum: string }) => {
-
-  const linkStyle = "hover:text-primary"
   return (
     <>
-      <Link to="/" className={linkStyle}>Home</Link>
-      <Link to="/projects" className={linkStyle}>Progetti</Link>
-      <Link to="/contact" className={linkStyle}>Contattami</Link>
-      <a href={linkCurriculum} download={"CV-Smailen-Vargas-Frontend.pdf"} className={linkStyle}>
-        Download CV
-      </a>
+      <ul className="flex gap-4">
+        <LinkComponent linkTo="/">Home</LinkComponent>
+        <LinkComponent linkTo="/projects">Progetti</LinkComponent>
+        <LinkComponent linkTo="/contact">Contattami</LinkComponent>
+        <LinkComponent
+          downloadable linkTo={linkCurriculum}
+          downloadName="CV-Smailen-Vargas-Frontend.pdf"
+        >
+          Download CV
+        </LinkComponent>
+      </ul>
     </>
   );
 };
