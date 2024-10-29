f3bd14b2ec76c0045a5513e9cf9c90aad95885b4
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-10-29 15:38:54 +0100

    refactor: :lipstick: aggiorna classe colori
    aggiorna i colori con la nuova classe variabile

--- src/Components/organisms/Layout.tsx
+++ src/Components/organisms/Layout.tsx
@@ -7,7 +7,7 @@ export const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <NavbarSample />
-      <div className="min-h-screen bg-neutral-100 py-16 xl:flex xl:justify-center">
+      <div className="min-h-screen bg-background py-16 xl:flex xl:justify-center">
         <main className="mt-20 flex flex-col justify-around gap-20 px-6 xl:max-w-[1024px]">
           {children}
         </main>
