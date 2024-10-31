3bdcf4632371820f6519a63fecb73fd87b714c88
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-10-31 13:28:12 +0100

    refactor: :speech_balloon: aggiorna presentazione per maggior descrizione

--- src/Components/organisms/Presentation.tsx
+++ src/Components/organisms/Presentation.tsx
@@ -5,19 +5,27 @@ const Presentation = () => {
         Presentazione
       </h2>
       <p>
-        Ciao, mi chiamo Smailen, <strong>frontend developer </strong>
-        appassionato di creare interfacce utente intuitive e coinvolgenti.
+        Sono un <strong>frontend developer</strong>. Ho
+        iniziato a programmare da bambino, con uno ZX Spectrum e vari manuali di
+        Basic, creando piccoli giochi e programmi, e da allora il mio interesse
+        per la tecnologia non si è mai fermato.
       </p>
       <p>
-        Con competenze in <strong>React</strong>, <strong>Vite</strong> e
-        <strong> Tailwind CSS</strong>, trasformo idee in esperienze digitali
-        fluide. Sono sempre alla ricerca di nuove tecnologie e puntualmente
-        migliorare le mie competenze per
-        <strong> diventare full-stack developer</strong>.
+        Negli anni ho esplorato vari aspetti creativi della tecnologia: dalla{" "}
+        <strong>modellazione 3D</strong> e il <strong>disegno digitale</strong>{" "}
+        al <strong>mondo di Arduino</strong>. Dopo esperienze in vari settori,
+        ho scelto di focalizzarmi sullo sviluppo <strong>frontend</strong>, dove
+        posso dare forma a idee trasformandole in applicazioni web fluide e ben
+        strutturate.
       </p>
       <p>
-        Dai un&apos;occhiata ai miei progetti per vedere come posso essere di
-        valore per il tuo team.
+        Il <strong>frontend</strong> rappresenta per me il perfetto equilibrio tra{" "}
+        <strong>programmazione</strong> e <strong>design</strong>, consentendomi
+        di tradurre progetti in esperienze digitali intuitive e dinamiche.
+      </p>
+      <p>
+        Dai un'occhiata ai miei progetti per vedere come posso contribuire al
+        tuo team e portare valore con le mie competenze e la mia passione.
       </p>
     </section>
   );
