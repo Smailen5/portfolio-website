0ff8516201ec37a2b72dc79daf0686cb466b90a8
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-11-04 12:18:55 +0100

    refactor: :recycle: migliora semantica componente
    aggiungo header e lista non ordinata per le skills

--- src/Components/molecules/Skills.tsx
+++ src/Components/molecules/Skills.tsx
@@ -1,17 +1,32 @@
-import { SiNextui } from "react-icons/si";
+// import { SiNextui } from "react-icons/si";
 import skillsData from "../../data/skillsData";
 import Skill from "../atoms/Skill";
-const Skills = () => {
+
+interface SkillsProps {
+  title: string;
+}
+const Skills = ({ title }: SkillsProps) => {
   return (
     <>
-      {skillsData.map((skill) => {
+      <div className="space-y-4">
+        <header>
+          <h3 className="text-xl font-semibold">{title}</h3>
+        </header>
+        <ul className="grid grid-cols-4 gap-2">
+          {skillsData.map((skill) => (
+            <Skill key={skill.name} {...skill} />
+          ))}
+        </ul>
+      </div>
+
+      {/* {skillsData.map((skill) => {
         return <Skill key={skill.name} {...skill} />;
-      })}
+      })} */}
       {/* questa icona viene importata diversamente dalle altre */}
-      <div className="flex items-center justify-between rounded-md bg-card p-4 uppercase shadow-md shadow-secondary">
+      {/* <div className="flex items-center justify-between rounded-md bg-card p-4 uppercase shadow-md shadow-secondary">
         <p className="font-semibold uppercase">NextUI</p>
         <SiNextui size={48} className="" />
-      </div>
+      </div> */}
     </>
   );
 };
