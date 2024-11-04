18deb4c5763a02c5ccec3cbe32030448b63da25a
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-11-04 14:01:47 +0100

    refactor: :lipstick: imposta dimensione fissa immagini

--- src/Components/atoms/Skill.tsx
+++ src/Components/atoms/Skill.tsx
@@ -10,12 +10,14 @@ const Skills = ({ name, icon }: { name: string; icon: string }) => {
   const { useWindowWidth } = useGlobalContext();
   const width = useWindowWidth();
 
+  const iconSize = "w-12 h-12"
+
   if (width < 720) {
     return (
       <>
-        <li className="flex items-center justify-between rounded-md bg-card p-2">
+        <li className="flex items-center justify-between rounded-md bg-card p-2 mb-4">
           <p className="font-semibold uppercase">{name}</p>
-          <img src={icon} alt={name + "icon"} />
+          <img src={icon} alt={name + "icon"} className={`${iconSize}`} />
         </li>
       </>
     );
@@ -28,7 +30,7 @@ const Skills = ({ name, icon }: { name: string; icon: string }) => {
         <TooltipProvider>
           <Tooltip>
             <TooltipTrigger>
-              <img src={icon} alt={name + " icon"} />
+              <img src={icon} alt={name + " icon"} className={`${iconSize}`} />
             </TooltipTrigger>
             <TooltipContent>{name}</TooltipContent>
           </Tooltip>
