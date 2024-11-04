896984c6698acf874b3e80a58e60fcea872e054e
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-11-04 15:31:17 +0100

    refactor: :lipstick: test organizzazione sezione
    testo come si vede la nuova sezione su diversi dispositivi. Non mi piace

--- src/Components/organisms/Socials.tsx
+++ src/Components/organisms/Socials.tsx
@@ -1,14 +1,24 @@
 import social from "../../data/social";
+import { HeroContact } from "../atoms/HeroContact";
+// import { HeroContact } from "../atoms/HeroContact";
 import ButtonSocial from "../molecules/ButtonSocial";
+import FormComponent from "./FormComponent";
 
 const Socials = () => {
   return (
     <section className="flex flex-col gap-8">
       <h3 className="text-center text-2xl font-semibold">Social</h3>
-      <div className="flex h-full w-full flex-col justify-start gap-8 md:grid md:grid-cols-3">
-        {social.map((el) => (
-          <ButtonSocial key={el.name} {...el} />
-        ))}
+      <div className="grid md:grid-cols-2">
+        <div>
+          <HeroContact />
+          <div className="flex flex-col gap-8 md:grid md:grid-cols-2 md:gap-0">
+            {social.map((el, index) => (
+              <ButtonSocial key={index} {...el} />
+            ))}
+          </div>
+        </div>
+        {/* <HeroContact/> */}
+        <FormComponent />
       </div>
     </section>
   );
