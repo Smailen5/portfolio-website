14f56be8cb05dde3255f6d0cfed6d5eb3f5bf3e6
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-11-04 14:02:41 +0100

    refactor: :truck: esporta nuove icone

--- src/data/skillsData.ts
+++ src/data/skillsData.ts
@@ -1,3 +1,8 @@
+import axiosIcon from "../assets/icons/axios.svg";
+import formikIcon from "../assets/icons/formik.svg";
+import formspreeIcon from "../assets/icons/formspree.svg";
+import githubIcon from "../assets/icons/GitHubIcon.svg";
+import bootstrapIcon from "../assets/icons/icons8-bootstrap.svg";
 import css3Icon from "../assets/icons/icons8-css3.svg";
 import typescriptIcon from "../assets/icons/icons8-dattiloscritto.svg";
 import gitIcon from "../assets/icons/icons8-git.svg";
@@ -5,6 +10,9 @@ import html5Icon from "../assets/icons/icons8-html5.svg";
 import jsIcon from "../assets/icons/icons8-javascript-48.svg";
 import reactIcon from "../assets/icons/icons8-react.svg";
 import tailwindIcon from "../assets/icons/icons8-tailwind-css.svg";
+import netlifyIcon from "../assets/icons/netlify.svg";
+import nextuiIcon from "../assets/icons/nextjs.svg";
+import shadcnIcon from "../assets/icons/shadcn.svg";
 
 const icons = [
   {
@@ -44,4 +52,53 @@ export const program = [
   },
 ];
 
+export const framework = [
+  {
+    name: "React",
+    icon: reactIcon,
+  },
+  {
+    name: "Next ui",
+    icon: nextuiIcon,
+  },
+  {
+    name: "Shadcn ui",
+    icon: shadcnIcon,
+  },
+  {
+    name: "Formik",
+    icon: formikIcon,
+  },
+];
+
+export const library = [
+  {
+    name: "Tailwind",
+    icon: tailwindIcon,
+  },
+  {
+    name: "Bootstrap",
+    icon: bootstrapIcon,
+  },
+  {
+    name: "Git",
+    icon: gitIcon,
+  },
+  {
+    name: "Axios",
+    icon: axiosIcon,
+  },
+  {
+    name: "Form spree",
+    icon: formspreeIcon,
+  },
+  {
+    name: "GitHub",
+    icon: githubIcon,
+  },
+  {
+    name: "Netlify",
+    icon: netlifyIcon,
+  },
+];
 export default icons;
