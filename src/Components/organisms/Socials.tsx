01a0fbf76b8618b351958b6cc567dc42698055cf
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-11-04 15:01:19 +0100

    refactor: :recycle: elimina destruturazione
    utilizzo spread operator anziche la destrutturazione per rendere il codice piu snello

--- src/Components/organisms/Socials.tsx
+++ src/Components/organisms/Socials.tsx
@@ -6,8 +6,8 @@ const Socials = () => {
     <section className="flex flex-col gap-8">
       <h3 className="text-center text-2xl font-semibold">Social</h3>
       <div className="flex h-full w-full flex-col justify-start gap-8 md:grid md:grid-cols-3">
-        {social.map(({ name, link, icon }) => (
-          <ButtonSocial key={name} name={name} link={link} icon={icon} />
+        {social.map((el) => (
+          <ButtonSocial key={el.name} {...el} />
         ))}
       </div>
     </section>
