75527573cc0ab4c9b7fca2bc63fa689740c2802b
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-11-04 13:12:48 +0100

    refactor: :truck: esporta le icone con un nome

--- src/data/skillsData.ts
+++ src/data/skillsData.ts
@@ -1,7 +1,10 @@
+import css3Icon from "../assets/icons/icons8-css3.svg";
+import typescriptIcon from "../assets/icons/icons8-dattiloscritto.svg";
+import gitIcon from "../assets/icons/icons8-git.svg";
+import html5Icon from "../assets/icons/icons8-html5.svg";
 import jsIcon from "../assets/icons/icons8-javascript-48.svg";
 import reactIcon from "../assets/icons/icons8-react.svg";
 import tailwindIcon from "../assets/icons/icons8-tailwind-css.svg";
-import gitIcon from "../assets/icons/icons8-git.svg";
 
 const icons = [
   {
@@ -19,6 +22,25 @@ const icons = [
   {
     name: "Git",
     icon: gitIcon,
+  },
+];
+
+export const program = [
+  {
+    name: "HTML5",
+    icon: html5Icon,
+  },
+  {
+    name: "css3",
+    icon: css3Icon,
+  },
+  {
+    name: "Javascript",
+    icon: jsIcon,
+  },
+  {
+    name: "Typescript",
+    icon: typescriptIcon,
   },
 ];
 
