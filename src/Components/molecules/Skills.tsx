c2dcbbbc676d39ee37327a812e824b8c08872745
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-11-04 12:20:32 +0100

    refactor: :fire: rimuove codice commentato

--- src/Components/molecules/Skills.tsx
+++ src/Components/molecules/Skills.tsx
@@ -1,4 +1,3 @@
-// import { SiNextui } from "react-icons/si";
 import skillsData from "../../data/skillsData";
 import Skill from "../atoms/Skill";
 
@@ -18,15 +17,6 @@ const Skills = ({ title }: SkillsProps) => {
           ))}
         </ul>
       </div>
-
-      {/* {skillsData.map((skill) => {
-        return <Skill key={skill.name} {...skill} />;
-      })} */}
-      {/* questa icona viene importata diversamente dalle altre */}
-      {/* <div className="flex items-center justify-between rounded-md bg-card p-4 uppercase shadow-md shadow-secondary">
-        <p className="font-semibold uppercase">NextUI</p>
-        <SiNextui size={48} className="" />
-      </div> */}
     </>
   );
 };
