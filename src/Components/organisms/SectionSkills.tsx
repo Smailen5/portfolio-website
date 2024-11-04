44597809891c98c0cc42a46e9fd7ee0044f0f97f
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-11-04 14:02:27 +0100

    refactor: :truck: importa icone

--- src/Components/organisms/SectionSkills.tsx
+++ src/Components/organisms/SectionSkills.tsx
@@ -1,19 +1,22 @@
 import { ButtonAction } from "../atoms/ButtonAction";
-import { Section } from "../atoms/Section";
+// import { Section } from "../atoms/Section";
 import Skills from "../molecules/Skills";
+import { framework, library, program } from "../../data/skillsData";
 
 export const SectionSkill = () => {
-  const linguaggi = ["HTML5", "CSS3", "JavaScript", "TypeScript"];
+  // const linguaggi = ["Git", "React", "JavaScript", "Tailwind"];
   return (
     <section className="mx-auto flex w-full flex-col gap-8">
       <h3 className="text-center text-2xl font-semibold">
         Competenze acquisite
       </h3>
       <div className="flex h-full w-full flex-col flex-wrap gap-8 md:grid md:grid-cols-3">
-        <Skills />
+        <Skills program={program} title="Linguaggi" />
+        <Skills program={framework} title="Framework e librerie" />
+        <Skills program={library} title="Strumenti e Tecnologie " />
 
         {/* Questa e una prova */}
-        <Section title="prova di title" content={linguaggi} />
+        {/* <Section title="prova di title" content={skillsData} /> */}
         
         <div className="space-y-4 md:col-start-2">
           <h3 className="text-center text-2xl font-semibold">
