029d98bbb97cd715c5ab83bc2adbe6b141aed771
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-11-05 13:00:06 +0100

    refactor: :lipstick: aumenta spazio verticale e gap

--- src/Components/organisms/Socials.tsx
+++ src/Components/organisms/Socials.tsx
@@ -6,12 +6,12 @@ import FormComponent from "./FormComponent";
 
 const Socials = () => {
   return (
-    <section className="flex flex-col gap-8">
+    <section className="flex flex-col gap-12">
       <h3 className="text-center text-3xl font-semibold">Contatti</h3>
       <div className="grid md:grid-cols-2 gap-8">
         <div className="grid gap-8 md:gap-0">
           <HeroContact noHeader className="lg:w-full" />
-          <div className="flex flex-col gap-8 md:grid md:grid-cols-2 md:gap-0 md:items-center h-40">
+          <div className="flex flex-col gap-8 md:grid md:grid-cols-2 md:gap-0 md:gap-x-4 md:items-center h-40">
             {social.map((el, index) => (
               <ButtonSocial key={index} {...el} />
             ))}
