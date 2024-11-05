03ff11053e80199aff2984928779b1e186028f38
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-11-05 12:33:26 +0100

    refactor: :label: aggiunge parametro
    aggiungo parametro per rimuovere header della sezione contatti

--- src/Components/atoms/HeroContact.tsx
+++ src/Components/atoms/HeroContact.tsx
@@ -1,12 +1,18 @@
-export const HeroContact = () => {
+interface HeroContactProps {
+  noHeader?: boolean;
+}
+
+export const HeroContact = ({ noHeader }: HeroContactProps) => {
   return (
     <section
       aria-labelledby="contact-heading"
       className="flex-1 space-y-4 md:flex md:flex-col md:justify-center md:text-left lg:w-1/2 lg:text-left"
     >
-      <h1 id="contact-heading" className="text-3xl font-semibold">
-        Contattami
-      </h1>
+      {!noHeader && (
+        <h1 id="contact-heading" className="text-3xl font-semibold">
+          Contattami
+        </h1>
+      )}
       <p>
         Se hai domande, richieste di collaborazione, o semplicemente vuoi
         parlare di React, non esitare a usare il modulo qui sotto per metterti
