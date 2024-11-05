d24e59bda0ba7db9fb743202ab9ea63b8e9a03e2
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-11-05 12:52:24 +0100

    refactor: :label: aggiunge parametro
    aggiungo tailwind merge per poter modificare le classi della section

--- src/Components/atoms/HeroContact.tsx
+++ src/Components/atoms/HeroContact.tsx
@@ -1,12 +1,15 @@
+import { twMerge } from "tailwind-merge";
+
 interface HeroContactProps {
   noHeader?: boolean;
+  className?: string;
 }
 
-export const HeroContact = ({ noHeader }: HeroContactProps) => {
+export const HeroContact = ({ noHeader, className }: HeroContactProps) => {
   return (
     <section
       aria-labelledby="contact-heading"
-      className="flex-1 space-y-4 md:flex md:flex-col md:justify-center md:text-left lg:w-1/2 lg:text-left"
+      className={twMerge("flex-1 space-y-4 md:flex md:flex-col md:justify-center md:text-left lg:w-1/2 lg:text-left", className)}
     >
       <h3 id="contact-heading" className="sr-only">
         Contattami
