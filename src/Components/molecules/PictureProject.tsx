20b09b48092e0834fecedecba439af1b0f0cc6d2
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-11-05 14:56:27 +0100

    fix: :adhesive_bandage: visualizza testo alternativo
    corregge la visualizzazione del testo alternativo dell'immagine quando l'immagine non e disponibile. Rimaneva lo skeleton in caricamento e veniva visualizzato il testo alternativo. Ora viene visualizzato il testo di immagine non disponibile

--- src/Components/molecules/PictureProject.tsx
+++ src/Components/molecules/PictureProject.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+// import { useState } from "react";
 import { LinkBlank } from "../atoms/LinkBlank";
 import { nameCorrect } from "@/utils/nameCorrect";
 
@@ -6,23 +6,27 @@ type PictureProject = {
   linkSite: string;
   image?: string;
   name: string;
-  onLoad: ()=>void;
+  onLoad: () => void;
+  onError: () => void;
+  imageLoadFailed?: boolean;
 };
 
 export const PictureProject: React.FC<PictureProject> = ({
   linkSite,
   image,
   name,
-  onLoad
+  onLoad,
+  onError,
+  imageLoadFailed,
 }) => {
-  const [error, setError] = useState(false);
+  // const [error, setError] = useState(false);
 
-  const handleError = () => {
-    setError(true);
-  };
+  // const handleError = () => {
+  //   setError(true);
+  // };
 
   // Se non ho ancora caricato l'immagine
-  if (error) {
+  if (imageLoadFailed) {
     return (
       <div className="flex h-full min-h-[195px] w-full items-center justify-center rounded-md bg-muted text-muted-foreground md:min-h-[300px]">
         <p>Immagine non ancora disponibile</p>
@@ -36,7 +40,7 @@ export const PictureProject: React.FC<PictureProject> = ({
         <img
           loading="lazy"
           src={image}
-          onError={handleError}
+          onError={onError}
           onLoad={onLoad}
           // forse conviene dare una misura fissa a tutte le immagini per evitare dimensioni diverse es.`lg:w-[480px] lg:h-[300px]`
           className="object-cover shadow-lg transition-all duration-500 lg:hover:scale-110"
--- src/Components/organisms/CardProject.tsx
+++ src/Components/organisms/CardProject.tsx
@@ -18,19 +18,25 @@ export const CardProject: React.FC<CardProjectType> = ({
   image,
   technologies,
 }) => {
-  const [imgLoad, setImgLoad] = useState(false);
+  const [imageLoad, setImgLoad] = useState(false);
+  const [error, setError] = useState(false);
+
+  // if (error) setImgLoad(false);
   return (
     <>
       {/* PROGETTO SINGOLO */}
       <article className="flex flex-col gap-4 rounded-md border border-border bg-background text-foreground shadow-md">
         {/* Skeleton immagine */}
-        {!imgLoad && <Skeleton height={250} />}
+        {!imageLoad && !error && <Skeleton height={250} />}
+
         {/* immagine */}
         <PictureProject
           linkSite={html_url}
           image={image}
           name={name}
           onLoad={() => setImgLoad(true)}
+          onError={() => setError(true)}
+          imageLoadFailed={error}
         />
         {/* contenitore nome progetto e descrizione */}
         <ContainerProject name={name} technologies={technologies}>
