3422abef40794deeabfeb843b1770a9ef028eb60
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-11-05 12:36:27 +0100

    refactor: :wheelchair: migliora accessibilita
    aggiunge heading per gli screen reader anche per la pagina home

--- src/Components/atoms/HeroContact.tsx
+++ src/Components/atoms/HeroContact.tsx
@@ -8,11 +8,10 @@ export const HeroContact = ({ noHeader }: HeroContactProps) => {
       aria-labelledby="contact-heading"
       className="flex-1 space-y-4 md:flex md:flex-col md:justify-center md:text-left lg:w-1/2 lg:text-left"
     >
-      {!noHeader && (
-        <h1 id="contact-heading" className="text-3xl font-semibold">
-          Contattami
-        </h1>
-      )}
+      <h3 id="contact-heading" className="sr-only">
+        Contattami
+      </h3>
+      {!noHeader && <h3 className="text-3xl font-semibold">Contattami</h3>}
       <p>
         Se hai domande, richieste di collaborazione, o semplicemente vuoi
         parlare di React, non esitare a usare il modulo qui sotto per metterti
