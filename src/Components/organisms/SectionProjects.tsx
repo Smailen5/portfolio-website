eb720477acc8165671cd6a8736a7c74ce4dd74ab
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-11-05 14:06:54 +0100

    refactor: :art: aggiorna componente per utilizzare SkeletonLoaderCard

--- src/Components/organisms/SectionProjects.tsx
+++ src/Components/organisms/SectionProjects.tsx
@@ -1,18 +1,23 @@
 import { useFetch } from "@/utils/useFetch";
-import { Loading } from "../atoms/Loading";
+// import { Loading } from "../atoms/Loading";
 import { CardProject } from "./CardProject";
+import { SkeletonLoaderCard } from "../atoms/SkeletonLoaderCard";
 
 export const SectionProjects = () => {
   const { projects, loading, error } = useFetch();
 
-  if (loading) return <Loading />;
+  // Cambia qui quante card skeleton visualizzare
+  const arraySkeleton = Array.from({ length: 4 });
+  // if (loading) return <SkeletonLoaderCard />;
   if (error) return <p>{error}</p>;
   return (
     <>
       <section className="grid gap-4 md:grid-cols-2">
-        {projects.map((project) => {
-          return <CardProject key={project.name} {...project} />;
-        })}
+        {loading
+          ? arraySkeleton.map((_, index) => <SkeletonLoaderCard key={index} />)
+          : projects.map((project) => {
+              return <CardProject key={project.name} {...project} />;
+            })}
       </section>
     </>
   );
