819e2647007e55fbc8830a63973cfa40ba0ebc95
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-11-06 14:34:11 +0100

    refactor: :art: utilizza compontenti Section e Header

--- src/Components/organisms/SectionSkills.tsx
+++ src/Components/organisms/SectionSkills.tsx
@@ -1,17 +1,18 @@
 import { framework, language, utility } from "../../data/skillsData";
+import { Header } from "../atoms/Header";
+import { Section } from "../atoms/Section";
 import Skills from "../molecules/Skills";
 
 export const SectionSkill = () => {
   return (
-    <section className="mx-auto flex w-full flex-col gap-12">
-      <h3 className="text-center text-3xl font-semibold">
-        Competenze acquisite
-      </h3>
-      <div className="flex h-full w-full flex-col flex-wrap gap-12 md:grid md:grid-cols-3">
+    <Section>
+      <Header>Competenze acquisite</Header>
+
+      <main className="flex h-full w-full flex-col flex-wrap gap-12 md:grid md:grid-cols-3">
         <Skills section={language} title="Linguaggi" />
         <Skills section={framework} title="Framework e Librerie" />
         <Skills section={utility} title="Strumenti di Sviluppo" />
-      </div>
-    </section>
+      </main>
+    </Section>
   );
 };
