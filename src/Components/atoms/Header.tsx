738967098c26465f9b870b6fe72e3fbc9af13af3
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-11-06 14:59:23 +0100

    refactor: :beers: dimensione heading dinamico
    aggiunge switch per cambiare dinamicamente la grandezza del heading

--- src/Components/atoms/Header.tsx
+++ src/Components/atoms/Header.tsx
@@ -1,20 +1,38 @@
+import React from "react";
 import { twMerge } from "tailwind-merge";
 
 interface HeaderProps {
   children: string;
   titleID?: string;
   className?: string;
+  type: "h1" | "h2" | "h3" | "h4";
 }
 
-export const Header = ({ children, titleID, className }: HeaderProps) => {
+export const Header = ({ children, titleID, className, type="h1" }: HeaderProps) => {
+    // Un po to much, ma sono stanco quindi va bene
+    let textSize
+    switch (type) {
+        case "h1": textSize ="text-4xl" 
+            break;
+        case "h2": textSize ="text-3xl";
+            break;
+        case "h3": textSize ="text-2xl";
+            break;
+        case "h4": textSize ="text-xl";
+            break;
+        default: textSize ="text-lg"
+            break;
+    }
   return (
     <header>
-      <h3
-        id={titleID || children}
-        className={twMerge("text-center text-3xl font-semibold", className)}
-      >
-        {children}
-      </h3>
+      {React.createElement(
+        type,
+        {
+          id: titleID || children,
+          className: twMerge(`text-center ${textSize} font-semibold`, className),
+        },
+        children,
+      )}
     </header>
   );
 };
