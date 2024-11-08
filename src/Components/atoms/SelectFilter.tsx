e06eb70d1b5541c8ee117cf340a83f7111ec3aff
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-11-08 13:00:52 +0100

    refactor: :fire: elimina codice di test

--- src/Components/atoms/SelectFilter.tsx
+++ src/Components/atoms/SelectFilter.tsx
@@ -1,29 +1,14 @@
-import { Label, Option, Select, Selection } from "./SelectCustom";
+import { Option, Selection } from "./SelectCustom";
 
 export const SelectFilter = () => {
   return (
     <>
       <div className="flex flex-col">
-        <label htmlFor="tech">Filtra per tecnologia</label>
-        <select name="tech" id="tech" className="rounded-md bg-green-500">
-          <option value="" disabled>
-            --Seleziona una opzione
-          </option>
-          <option value="html" className="">
-            html
-          </option>
-          <option value="css">css</option>
-          <option value="javascript">javascript</option>
-          <option value="typescript">typescript</option>
-          <option value="react" disabled>
-            react
-          </option>
-        </select>
         <Selection label="prova di label" name="opzioni">
-            <Option>prova </Option>
-            <Option>prova 2</Option>
-            <Option>prova 3</Option>
-            <Option>prova 4</Option>
+          <Option>prova </Option>
+          <Option>prova 2</Option>
+          <Option>prova 3</Option>
+          <Option>prova 4</Option>
         </Selection>
       </div>
     </>
