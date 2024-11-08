eca11919042f3a5903c4f37166e8455bb32d5151
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-11-08 15:15:36 +0100

    refactor: :construction: passa le props ai componenti

--- src/Components/atoms/SelectFilter.tsx
+++ src/Components/atoms/SelectFilter.tsx
@@ -1,17 +1,26 @@
+import { Dispatch, SetStateAction } from "react";
 import { Option, Selection } from "./SelectCustom";
 
-export const SelectFilter = () => {
+export const SelectFilter = ({
+  onSelectionChange,
+}: {
+  onSelectionChange: Dispatch<SetStateAction<string>>;
+}) => {
   return (
     <>
       {/* <div className="flex flex-col"> */}
-        <Selection label="prova di label" name="opzioni">
-          <Option>Html 5 </Option>
-          <Option>CSS 3</Option>
-          <Option>JavaScript</Option>
-          <Option>TypeScript</Option>
-          <Option>Tailwind CSS</Option>
-          <Option>React</Option>
-        </Selection>
+      <Selection
+        label="prova di label"
+        name="opzioni"
+        onSelectionChange={onSelectionChange}
+      >
+        <Option>Html 5 </Option>
+        <Option>CSS 3</Option>
+        <Option>JavaScript</Option>
+        <Option>TypeScript</Option>
+        <Option>Tailwind CSS</Option>
+        <Option>React</Option>
+      </Selection>
       {/* </div> */}
     </>
   );
--- src/Components/molecules/Filter.tsx
+++ src/Components/molecules/Filter.tsx
@@ -1,11 +1,17 @@
+import { useState } from "react";
 import { Button } from "../atoms/button";
 import { SelectFilter } from "../atoms/SelectFilter";
 
 export const Filter = () => {
+  const [selectedFilter,setSelectedFilter] = useState('Tutto')
+
+  const handleSearch = ()=>{
+    console.log("filtra progetti per:", selectedFilter)
+  }
   return (
     <section className="w-full bg-red-500 flex items-center gap-4">
-      <SelectFilter />
-      <Button>Cerca</Button>
+      <SelectFilter onSelectionChange={setSelectedFilter}/>
+      <Button onClick={handleSearch}>Cerca</Button>
     </section>
   );
 };
