f707a5d42078dce5639e3fdaecb1ff102ba6c93a
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-11-11 15:05:39 +0100

    refactor: :fire: rimuove codice commentato

--- src/Components/atoms/SelectFilter.tsx
+++ src/Components/atoms/SelectFilter.tsx
@@ -14,14 +14,11 @@ export const SelectFilter = ({
         onSelectionChange={onSelectionChange}
       >
         <Option>Tutto</Option>
-        {/* disabilita bottone html5 o rimuovilo */}
-        {/* <Option>HTML5</Option> */}
         <Option>CSS</Option>
         <Option>JavaScript</Option>
         <Option>TypeScript</Option>
         <Option>Tailwind</Option>
         <Option>React</Option>
-        {/* <Option>Work in progress</Option> */}
       </Selection>
     </>
   );
