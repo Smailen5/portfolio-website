6f5994e8de4f27224e44bcbe40e65acd246733a5
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-11-11 14:32:52 +0100

    refactor: :speech_balloon: aggiorna testo

--- src/Components/molecules/Filter.tsx
+++ src/Components/molecules/Filter.tsx
@@ -10,7 +10,7 @@ export const Filter = ({ setSelectedFilter, number }: FilterProps) => {
   return (
     <section className="flex w-full flex-col items-start gap-8 rounded-md bg-secondary p-2 text-secondary-foreground md:flex-row md:items-end md:justify-between md:gap-0">
       <SelectFilter onSelectionChange={setSelectedFilter} />
-      <p>progetti visualizzati: <span className="font-semibold">{number}</span></p>
+      <p>Progetti visualizzati: <span className="font-semibold">{number}</span></p>
     </section>
   );
 };
