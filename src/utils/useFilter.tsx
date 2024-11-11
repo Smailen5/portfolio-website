50d0cf1feb14a30ebbbc49af1ac80b0540710b25
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-11-11 15:13:26 +0100

    style: :fire: rimuove spazi
    rimuove log e spazi non piu utili

--- src/utils/useFilter.tsx
+++ src/utils/useFilter.tsx
@@ -5,21 +5,16 @@ export const useFilter = () => {
   const [selectedFilter, setSelectedFilter] = useState("Tutto");
   const { projects } = useFetch();
 
-
-    // Array filtrato
-    const filteredProjects =
-      selectedFilter === "Tutto"
-        ? projects
-        : projects.filter((project) =>
-            project.technologies.includes(selectedFilter),
-          );
-
+  // Array filtrato
+  const filteredProjects =
+    selectedFilter === "Tutto"
+      ? projects
+      : projects.filter((project) =>
+          project.technologies.includes(selectedFilter),
+        );
 
   // Numero progetti filtrati
   const numberFilteredProjects = filteredProjects.length;
-
-  console.log("progetti filtrati:", filteredProjects);
-  
 
   return {
     filteredProjects,
