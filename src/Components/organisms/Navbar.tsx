3094b8e562c39ce2a5e542800a5caad57e5f2df0
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-11-12 15:47:08 +0100

    refactor: :fire: rimuove codice inutilizzato

--- src/Components/organisms/Navbar.tsx
+++ src/Components/organisms/Navbar.tsx
@@ -1,14 +1,11 @@
 import { Navbar, NavbarBrand } from "@nextui-org/react";
 import curriculum from "../../assets/curriculum/Curriculum-Smailen-Vargas.pdf";
 import { useGlobalContext } from "../../utils/context";
-// import useChangeAvatar from "../../utils/useChangeAvatar";
+import { Avatar } from "../atoms/Avatar";
 import { DropdownNavbar } from "../molecules/DropdownNavbar";
 import { LinkNavbar } from "../molecules/LinkNavbar";
-import { Avatar } from "../atoms/Avatar";
 
 const NavbarSample = () => {
-  // const { currentAvatar, animation } = useChangeAvatar();
-
   const { useWindowWidth } = useGlobalContext();
   const windowWidth = useWindowWidth();
   const isDesktop = windowWidth >= 1024;
@@ -16,15 +13,7 @@ const NavbarSample = () => {
   return (
     <Navbar className="fixed left-0 top-0">
       <NavbarBrand>
-        {/* <Avatar
-          isBordered
-          color="primary"
-          src={currentAvatar}
-          name="SV"
-          alt="avatar icon"
-          className={`${animation} `}
-        /> */}
-        <Avatar name="Smailen Vargas"/>
+        <Avatar name="Smailen Vargas" />
       </NavbarBrand>
       {!isDesktop ? (
         <DropdownNavbar linkCurriculum={curriculum} />
