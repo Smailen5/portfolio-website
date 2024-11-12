7be40e1a449a2f7d327077994241edd10df2eb4b
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-11-12 15:44:27 +0100

    feat: :sparkles: crea funzione per estrarre iniziali nome

---
+++ src/utils/getInitials.ts
@@ -0,0 +1,10 @@
+// Estrae le iniziali da name
+// !Fa attenzione che non ci siano troppe iniziali
+export const getInitials = (name: string) => {
+  if (!name) return "";
+  const words = name.split(" ");
+  const initials = words
+    .slice(0, words.length)
+    .map((word) => word.charAt(0).toUpperCase());
+  return initials.join("");
+};
