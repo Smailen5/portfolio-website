bd1ea6b261d236580f36c8769aab1bb0ae20d441
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-11-13 14:13:57 +0100

    refactor: :lipstick: utilizza componente ModeToggle

--- src/Components/molecules/LinkNavbar.tsx
+++ src/Components/molecules/LinkNavbar.tsx
@@ -1,18 +1,21 @@
 import { LinkComponent } from "../atoms/LinkComponent";
+import { ModeToggle } from "./ModeToggle";
 
 export const LinkNavbar = ({ linkCurriculum }: { linkCurriculum: string }) => {
   return (
     <>
-      <ul className="flex gap-4">
+      <ul className="flex items-center gap-4">
         <LinkComponent linkTo="/">Home</LinkComponent>
         <LinkComponent linkTo="/projects">Progetti</LinkComponent>
         <LinkComponent linkTo="/contact">Contattami</LinkComponent>
         <LinkComponent
-          downloadable linkTo={linkCurriculum}
+          downloadable
+          linkTo={linkCurriculum}
           downloadName="CV-Smailen-Vargas-Frontend.pdf"
         >
           Download CV
         </LinkComponent>
+        <ModeToggle />
       </ul>
     </>
   );
--- src/Components/organisms/Navbar.tsx
+++ src/Components/organisms/Navbar.tsx
@@ -4,6 +4,7 @@ import { useGlobalContext } from "../../utils/context";
 import { Avatar } from "../atoms/Avatar";
 import { DropdownNavbar } from "../molecules/DropdownNavbar";
 import { LinkNavbar } from "../molecules/LinkNavbar";
+import { ModeToggle } from "../molecules/ModeToggle";
 
 const NavbarSample = () => {
   const { useWindowWidth } = useGlobalContext();
@@ -16,9 +17,15 @@ const NavbarSample = () => {
         <Avatar name="Smailen Vargas" />
       </NavbarBrand>
       {!isDesktop ? (
+        <>
+        <ModeToggle />
         <DropdownNavbar linkCurriculum={curriculum} />
+        </>
       ) : (
+        <>
         <LinkNavbar linkCurriculum={curriculum} />
+        {/* <ModeToggle /> */}
+        </>
       )}
     </Navbar>
   );
