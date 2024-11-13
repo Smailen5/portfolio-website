990a80501d3685d103cd04ab2475031fcd25093b
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2024-11-13 14:45:41 +0100

    fix: :lipstick: rimuove classi superflue

--- src/Components/organisms/Layout.tsx
+++ src/Components/organisms/Layout.tsx
@@ -7,7 +7,7 @@ export const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
       <NavbarSample />
-      <div className="flex h-auto w-full flex-col items-center bg-background">
+      <div className="flex h-auto w-full justify-center bg-background">
         <main className="flex w-full max-w-[1024px] flex-col flex-nowrap items-center justify-center gap-20 px-6 pb-14 pt-36">
           {children}
         </main>
