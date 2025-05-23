15e2e2eb1072a96ccef85d363e5809bd633b525f
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-05-23 16:17:31 +0200

    chore: 🧹 aggiorna l'importazione
    - aggiorna l'importazione del componente Button nel ButtonsProject per utilizzare il nuovo alias definito nel tsconfig

--- src/Components/molecules/ButtonsProject.tsx
+++ src/Components/molecules/ButtonsProject.tsx
@@ -1,24 +1,21 @@
-import { Button } from "@/Components/atoms/button";
+import { Button } from "@/components/atoms/Button";
 import { useNavigate } from "react-router-dom";
 
 type ButtonsProjectType = {
-  nome:string;
+  nome: string;
 };
 
-export const ButtonsProject: React.FC<ButtonsProjectType> = ({
-  nome,
-}) => {
+export const ButtonsProject: React.FC<ButtonsProjectType> = ({ nome }) => {
+  const navigate = useNavigate();
 
-  const navigate = useNavigate()
-
-  const goToProject = (nome:string)=>{
-    navigate(`/projects/${nome}`)
-  }
+  const goToProject = (nome: string) => {
+    navigate(`/projects/${nome}`);
+  };
 
   return (
     <div className="space-x-8">
-      <Button variant="outline" onClick={()=> goToProject(nome)}>
-          dettagli
+      <Button variant="outline" onClick={() => goToProject(nome)}>
+        dettagli
       </Button>
     </div>
   );
