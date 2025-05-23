228d44ccdde1bd2aac46f08a56dd11d4ee5cf797
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-05-23 16:19:36 +0200

    fix: 🩹 aggiorna l'importazione del componente Button nel ModeToggle
    - modificato l'import per utilizzare il nuovo nome con la lettera maiuscola
    - riorganizzata la struttura del componente per migliorare la leggibilità

--- src/Components/molecules/ModeToggle.tsx
+++ src/Components/molecules/ModeToggle.tsx
@@ -1,6 +1,6 @@
-import { Moon, Sun } from "lucide-react";
-import { Button } from "@/Components/atoms/button";
+import { Button } from "@/components/atoms/Button";
 import { useTheme } from "@/utils/themeProvider";
+import { Moon, Sun } from "lucide-react";
 
 export function ModeToggle() {
   const { theme, setTheme } = useTheme();
@@ -10,7 +10,13 @@ export function ModeToggle() {
   };
 
   return (
-    <Button tabIndex={0} onClick={handleTheme} variant="outline" size="icon" className="bg-transparent">
+    <Button
+      tabIndex={0}
+      onClick={handleTheme}
+      variant="outline"
+      size="icon"
+      className="bg-transparent"
+    >
       <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
       <span className="sr-only">Toggle theme</span>
