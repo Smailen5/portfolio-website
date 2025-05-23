9f9e654088a7d5d20488872d272d711237e690a0
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-05-23 16:19:53 +0200

    fix: 🩹 aggiorna l'importazione del componente Button nel SidebarMenu
    - modificato l'import per utilizzare il nuovo nome con la lettera maiuscola, migliorando la coerenza delle importazioni

--- src/Components/molecules/SidebarMenu.tsx
+++ src/Components/molecules/SidebarMenu.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import { Button } from "../atoms/button";
+import { Button } from "../atoms/Button";
 import { CurriculumDownload } from "./CurriculumDownload";
 import { ModeToggle } from "./ModeToggle";
 
