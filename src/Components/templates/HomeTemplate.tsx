6e3489fc97e8fcc3904736b2b72f54eaad9ce3f1
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-05-27 13:44:57 +0200

    chore: 🧹 aggiorna i percorsi di importazione e rinomina il componente SectionSkills
    - modificati gli import nel file HomeTemplate.tsx per riflettere la nuova struttura delle cartelle.
    - rinominato il componente SectionSkills.tsx in favore di Section.

--- src/Components/templates/HomeTemplate.tsx
+++ src/Components/templates/HomeTemplate.tsx
@@ -1,7 +1,7 @@
 import Presentation from "@features/about/components/Presentation";
 import { Hero } from "@features/home/components/Hero";
 import { LastProjects } from "@/features/projects/components/Latest";
-import { SectionSkill } from "@features/skills/components/SectionSkills";
+import { SectionSkill } from "@/features/skills/components/Section";
 import Socials from "@features/social/components/Socials";
 
 export const HomeTemplate = () => {
