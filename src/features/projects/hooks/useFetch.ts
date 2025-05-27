48e0df5341a242cbf82d080e613cf88f9f079e65
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-05-27 15:49:39 +0200

    feat: 🎉 migliora l'hook useFetch per il recupero dei progetti
    - aggiunta la gestione degli effetti collaterali con useEffect per il recupero dei progetti.
    - implementata la logica per il recupero dei progetti dall'API se non presenti nel session storage.
    - migliorata la gestione del caching con timestamp e logging per il debug.

--- src/features/projects/hooks/useFetch.ts
+++ src/features/projects/hooks/useFetch.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { projectService } from "../services/projectService";
 
 type Project = {
@@ -10,39 +10,62 @@ type Project = {
   technologies: string[];
 };
 
-export const useFetch = async () => {
+export const useFetch = () => {
   const [projects, setProjects] = useState<Project[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
 
   const cachedProjects = sessionStorage.getItem("projects");
   const cachedDuration = 1000 * 60 * 60;
 
-  // Controlla se i progetti sono salvati nel session storage e li recupera
-  if (cachedProjects) {
-    try {
-      const cachedData = JSON.parse(cachedProjects);
+  useEffect(() => {
+    const fetchProjects = async () => {
+      // Controlla se i progetti sono salvati nel session storage e li recupera
+      if (cachedProjects) {
+        try {
+          const cachedData = JSON.parse(cachedProjects);
 
-      // Verifica che il timestamp sia valido e che i progetti non siano più vecchi di un'ora
-      if (
-        cachedData.timestamp &&
-        Date.now() - cachedData.timestamp < cachedDuration
-      ) {
-        setProjects(cachedData.projects);
-        setLoading(false);
-        return;
+          // Verifica che il timestamp sia valido e che i progetti non siano più vecchi di un'ora
+          if (
+            cachedData.timestamp &&
+            Date.now() - cachedData.timestamp < cachedDuration
+          ) {
+            console.log("Progetti recuperati dalla cache");
+            setProjects(cachedData.projects);
+            console.log(projects);
+            setLoading(false);
+            return;
+          }
+        } catch (error) {
+          console.warn("Errore nel parsing della cache:", error);
+        }
       }
-    } catch (error) {
-      console.warn("Errore nel parsing della cache:", error);
-    }
-  }
 
-  try {
-    const response = await projectService.getAll();
-    setProjects(response.data);
-    setLoading(false);
-    console.log(projects);
-  } catch (error) {
-    console.warn("Errore nel recupero dei progetti:", error);
-  }
+      // Se non ci sono progetti nel session storage, li recupera dall'API
+      try {
+        console.log("Inizio recupero dei progetti dall'API");
+        const response = await projectService.getAll();
+        console.log(
+          "Risposta ricevuta: ",
+          response.status,
+          response.statusText,
+        );
+        if (response.status === 200) {
+          // Salva i progetti nel session storage aggiungendo il timestamp
+          sessionStorage.setItem(
+            "projects",
+            JSON.stringify({ projects: response.data, timestamp: Date.now() }),
+          );
+          // Salva i progetti nello stato
+          setProjects(response.data);
+          setLoading(false);
+          console.log(projects);
+        }
+      } catch (error) {
+        console.warn("Errore nel recupero dei progetti:", error);
+      }
+      return { projects, loading };
+    };
+    fetchProjects();
+  }, []);
   return { projects, loading };
 };
