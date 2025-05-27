53968f2859356c84d8f1b42960c2b1e62ebd8936
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-05-27 15:11:29 +0200

    feat: 🎉 aggiungi costanti API per la gestione degli endpoint
    - creato un nuovo file `api.ts` per definire gli endpoint API relativi all'autenticazione e ai progetti.
    - inclusi gli endpoint per login, recupero, creazione, aggiornamento e cancellazione dei progetti, oltre alla sincronizzazione con GitHub.

---
+++ src/shared/constants/api.ts
@@ -0,0 +1,17 @@
+const API_URL = import.meta.env.VITE_API_URL;
+
+export const API_ENDPOINTS = {
+  // AUTH
+  AUTH: {
+    LOGIN: `${API_URL}/api/users/login`,
+  },
+  // PROJECTS
+  PROJECTS: {
+    GET_ALL: `${API_URL}/api/projects`,
+    GET_BY_ID: `${API_URL}/api/projects/:id`,
+    CREATE: `${API_URL}/api/projects`,
+    UPDATE: `${API_URL}/api/projects/:id`,
+    DELETE: `${API_URL}/api/projects/:id`,
+    SYNC: `${API_URL}/api/github/sync`,
+  },
+};
