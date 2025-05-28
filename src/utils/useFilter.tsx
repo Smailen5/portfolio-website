980c54b7267aa25b6f035dab3f78b2b9e6d944ff
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-05-28 12:15:10 +0200

    refactor: 🔧 aggiorna l'hook useFilter per utilizzare il contesto globale
    - modificato l'importazione dell'hook `useFetch` per utilizzare il contesto globale.
    - implementata la logica per il recupero dei progetti dalla cache e dal contesto, migliorando il logging.
    - rimosso il codice ridondante per il caricamento dei progetti, semplificando la gestione dello stato.

--- src/utils/useFilter.tsx
+++ src/utils/useFilter.tsx
@@ -1,9 +1,21 @@
-import { useFetch } from "@/utils/useFetch";
+import { useFetch } from "@/features/projects/hooks/useFetch";
 import { useState } from "react";
+import { useGlobalContext } from './context';
 
 export const useFilter = () => {
   const [selectedFilter, setSelectedFilter] = useState("Tutto");
-  const { projects } = useFetch();
+  const { projects, setProjects } = useGlobalContext();
+
+  const cachedProjects = sessionStorage.getItem("projects");
+
+  if (cachedProjects) {
+    console.log("cachedProjects", cachedProjects);
+    setProjects(JSON.parse(cachedProjects));
+  } else {
+    const { projects } = useFetch();
+    // setProjects(projects);
+    console.log("projects", projects);
+  }
 
   // Array filtrato
   const filteredProjects =
