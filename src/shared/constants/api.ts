ec742b75352d3f4c60cfb37ae54d942d317330cf
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-06-23 19:00:38 +0200

    fix: 🔧 aggiorna l'endpoint GET_ALL per i progetti
    - modificato l'endpoint `GET_ALL` per puntare a una funzione Netlify locale.
    - migliora la configurazione per lo sviluppo locale e facilita il recupero dei progetti.

--- src/shared/constants/api.ts
+++ src/shared/constants/api.ts
@@ -7,7 +7,7 @@ export const API_ENDPOINTS = {
   },
   // PROJECTS
   PROJECTS: {
-    GET_ALL: `${API_URL}/api/projects`,
+    GET_ALL: `http://localhost:8888/.netlify/functions/get-projects/`,
     GET_BY_ID: `${API_URL}/api/projects/:id`,
     CREATE: `${API_URL}/api/projects`,
     UPDATE: `${API_URL}/api/projects/:id`,
