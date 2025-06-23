0a0f1bcd0e56b0c6e3f3eb21a8c052a22ac37de3
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-06-23 19:11:24 +0200

    refactor: ♻️ rimuove l'uso di API_URL e semplifica gli endpoint dei progetti
    - eliminati gli endpoint di autenticazione e aggiornati gli endpoint dei progetti per utilizzare un URL statico per lo sviluppo locale.
    - migliora la chiarezza e la gestione degli endpoint nel file `api.ts`.

--- src/shared/constants/api.ts
+++ src/shared/constants/api.ts
@@ -1,17 +1,6 @@
-const API_URL = import.meta.env.VITE_API_URL;
-
 export const API_ENDPOINTS = {
-  // AUTH
-  AUTH: {
-    LOGIN: `${API_URL}/api/users/login`,
-  },
   // PROJECTS
   PROJECTS: {
     GET_ALL: `http://localhost:8888/.netlify/functions/get-projects/`,
-    GET_BY_ID: `${API_URL}/api/projects/:id`,
-    CREATE: `${API_URL}/api/projects`,
-    UPDATE: `${API_URL}/api/projects/:id`,
-    DELETE: `${API_URL}/api/projects/:id`,
-    SYNC: `${API_URL}/api/github/sync`,
   },
 };
