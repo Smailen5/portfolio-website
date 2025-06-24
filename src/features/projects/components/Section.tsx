5f5137c8eeab0321d2844a929ef347fea8a25553
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-06-24 12:02:01 +0200

    feat: ✨ aggiunge visualizzazione degli errori nella sezione progetti
    - integrata la gestione degli errori nella componente `SectionProjects`, mostrando un messaggio di errore tramite il componente `ErrorMessage` in caso di problemi durante il filtraggio dei progetti.
    - questa modifica migliora l'esperienza utente, fornendo feedback chiaro in caso di errori.

--- src/features/projects/components/Section.tsx
+++ src/features/projects/components/Section.tsx
@@ -1,3 +1,4 @@
+import { ErrorMessage } from "@/components/atoms/ErrorMessage";
 import { CardProject } from "@/features/projects/components/Card";
 import { SkeletonLoaderCard } from "@/features/projects/components/Skeleton";
 import { useFilter } from "@/features/projects/hooks/useFilter";
@@ -6,6 +7,7 @@ import { Filter } from "@features/projects/components/Filter";
 export const SectionProjects = () => {
   const {
     loading,
+    error,
     filteredProjects,
     setSelectedFilter,
     numberFilteredProjects,
@@ -19,8 +21,10 @@ export const SectionProjects = () => {
       />
       {numberFilteredProjects === 0 ? (
         <p>Nessun progetto trovato.</p>
+      ) : error ? (
+        <ErrorMessage>{error}</ErrorMessage>
       ) : (
-        <section className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-full">
+        <section className="grid w-full grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
           {loading
             ? filteredProjects.map((_, index) => (
                 <SkeletonLoaderCard key={index} />
