cd74d21c522701b6081b7d5cb08189bba93d5281
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-06-24 14:58:05 +0200

    refactor: 🚨 semplifica la definizione dei componenti in MarkdownRenderer
    - Rimosso il parametro `node` dai componenti personalizzati (h1, h2, h3, p, a, pre, img) per semplificare la loro implementazione e migliorare la leggibilità del codice.

--- src/components/atoms/MarkdownRenderer.tsx
+++ src/components/atoms/MarkdownRenderer.tsx
@@ -24,31 +24,31 @@ export const MarkdownRenderer: React.FC<MarkdownRendererProps> = ({
         rehypePlugins={[rehypeRaw]} // Plugin per HTML raw nel markdown
         components={{
           // Personalizzazione dei componenti markdown
-          h1: ({ node, ...props }) => (
+          h1: ({ ...props }) => (
             <h1 className="my-4 text-3xl font-bold" {...props} />
           ),
-          h2: ({ node, ...props }) => (
+          h2: ({ ...props }) => (
             <h2 className="my-3 text-2xl font-semibold" {...props} />
           ),
-          h3: ({ node, ...props }) => (
+          h3: ({ ...props }) => (
             <h3 className="my-2 text-xl font-semibold" {...props} />
           ),
-          p: ({ node, ...props }) => <p className="my-2" {...props} />,
+          p: ({ ...props }) => <p className="my-2" {...props} />,
           // Link che si aprono in una nuova tab
-          a: ({ node, ...props }) => (
+          a: ({ ...props }) => (
             <LinkBlank isText href={props.href} {...props}>
               {props.children}
             </LinkBlank>
           ),
           // Blocchi di codice con stile personalizzato
-          pre: ({ node, ...props }) => (
+          pre: ({ ...props }) => (
             <pre
               className="my-4 overflow-auto rounded-lg bg-muted p-4 text-muted-foreground"
               {...props}
             />
           ),
           // Immagini con percorso personalizzato e stile responsive
-          img: ({ node, ...props }) => (
+          img: ({ ...props }) => (
             <img
               src={`${urlImageScreen}${nome}/${props.src}`}
               className="mb-4 w-full rounded-md md:w-auto object-contain"
