e6f836dcee483ec58b92da1c4b61168d416d42a3
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-06-24 18:06:11 +0200

    refactor: ♻️ rimuovi gli endpoint API hardcoded e utilizza variabili ambientali
    - eliminati gli endpoint hardcoded per l'autenticazione e la gestione dei progetti.
    - aggiornato il file `api.ts` per utilizzare le variabili ambientali definite nel file `env.ts`, migliorando la configurabilità dell'applicazione.

--- src/shared/constants/api.ts
+++ src/shared/constants/api.ts
@@ -1,12 +1,6 @@
-const API_URL = import.meta.env.VITE_API_URL;
 import { env } from "@/shared/utils/env";
 
 export const API_ENDPOINTS = {
-  // PROJECTS
-  PROJECTS: {
-    GET_ALL: `${API_BASE_URL}/.netlify/functions/get-projects/`,
-  },
-  // GITHUB
   GITHUB: {
     REPO: `${env.GITHUB_API_BASE}/repos/${env.GITHUB_USERNAME}/${env.GITHUB_REPO}`,
     CONTENTS: `${env.GITHUB_API_BASE}/repos/${env.GITHUB_USERNAME}/${env.GITHUB_REPO}/contents`,
