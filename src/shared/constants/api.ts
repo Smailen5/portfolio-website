4dc763531e33a3fbb246325a41122a412971c777
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-06-24 18:06:11 +0200

    refactor: ♻️ rimuovi gli endpoint API hardcoded e utilizza variabili ambientali
    - eliminati gli endpoint hardcoded per l'autenticazione e la gestione dei progetti.
    - aggiornato il file `api.ts` per utilizzare le variabili ambientali definite nel file `env.ts`, migliorando la configurabilità dell'applicazione.

--- src/shared/constants/api.ts
+++ src/shared/constants/api.ts
@@ -1,21 +1,6 @@
-const API_URL = import.meta.env.VITE_API_URL;
 import { env } from "@/shared/utils/env";
 
 export const API_ENDPOINTS = {
-  // AUTH
-  AUTH: {
-    LOGIN: `${API_URL}/api/users/login`,
-  },
-  // PROJECTS
-  PROJECTS: {
-    GET_ALL: `${API_URL}/api/projects`,
-    GET_BY_ID: `${API_URL}/api/projects/:id`,
-    CREATE: `${API_URL}/api/projects`,
-    UPDATE: `${API_URL}/api/projects/:id`,
-    DELETE: `${API_URL}/api/projects/:id`,
-    SYNC: `${API_URL}/api/github/sync`,
-  },
-  // GITHUB
   GITHUB: {
     REPO: `${env.GITHUB_API_BASE}/repos/${env.GITHUB_USERNAME}/${env.GITHUB_REPO}`,
     CONTENTS: `${env.GITHUB_API_BASE}/repos/${env.GITHUB_USERNAME}/${env.GITHUB_REPO}/contents`,
