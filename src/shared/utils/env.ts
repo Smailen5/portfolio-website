2de670378e8e1bb11a78487cbb4f8fa43dcc640b
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-06-24 19:11:35 +0200

    refactor: ♻️ aggiorna le variabili ambientali per l'integrazione con GitHub
    - sostituiti i token e gli endpoint hardcoded con variabili ambientali più sicure nel file `env.ts`, migliorando la configurabilità e la sicurezza dell'applicazione.

--- src/shared/utils/env.ts
+++ src/shared/utils/env.ts
@@ -5,13 +5,13 @@ VITE_API_PORT: import.meta.env.VITE_API_PORT,
 
 // ! Questa e da cambiare urgente perche e esposta
 FORM_SPREE_ACTION_URL: import.meta.env.VITE_APP_FORM_SPREE_ACTION_URL,
-GITHUB_TOKEN: import.meta.env.GITHUB_TOKEN,
+GITHUB_TOKEN: import.meta.env.VITE_GITHUB_TOKEN,
 
 // Altre configurazioni
 VITE_APP_ENV: import.meta.env.VITE_APP_ENV,
 
 // GitHub API endpoints
-GITHUB_API_BASE: import.meta.env.GITHUB_API_BASE,
-GITHUB_USERNAME: import.meta.env.GITHUB_USERNAME,
-GITHUB_REPO: import.meta.env.GITHUB_REPO,
+GITHUB_API_BASE: import.meta.env.VITE_GITHUB_API_BASE,
+GITHUB_USERNAME: import.meta.env.VITE_GITHUB_USERNAME,
+GITHUB_REPO: import.meta.env.VITE_GITHUB_REPO,
 }
