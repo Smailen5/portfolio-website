8aadb72a10307def87949ff6752fb9385c145433
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-06-24 18:04:19 +0200

    feat: 🎉 aggiungi il file di configurazione ambientale per l'applicazione
    - creato il file `env.ts` per gestire le variabili ambientali necessarie all'applicazione.
    - include configurazioni per l'API, il server locale e le credenziali di GitHub, migliorando la gestione delle impostazioni ambientali.

---
+++ src/shared/utils/env.ts
@@ -0,0 +1,17 @@
+export const env = {
+  // Configurazione server in locale
+VITE_API_URL: import.meta.env.VITE_API_URL,
+VITE_API_PORT: import.meta.env.VITE_API_PORT,
+
+// ! Questa e da cambiare urgente perche e esposta
+FORM_SPREE_ACTION_URL: import.meta.env.VITE_APP_FORM_SPREE_ACTION_URL,
+GITHUB_TOKEN: import.meta.env.GITHUB_TOKEN,
+
+// Altre configurazioni
+VITE_APP_ENV: import.meta.env.VITE_APP_ENV,
+
+// GitHub API endpoints
+GITHUB_API_BASE: import.meta.env.GITHUB_API_BASE,
+GITHUB_USERNAME: import.meta.env.GITHUB_USERNAME,
+GITHUB_REPO: import.meta.env.GITHUB_REPO,
+}
