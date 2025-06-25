e545aaaa70ea7db1a9368884c542e283bf3ec40e
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-06-25 19:59:50 +0200

    refactor: ♻️ aggiorna la gestione delle variabili ambientali nel file env.ts
    - aggiunto il tipo di ambiente dell'applicazione per migliorare la configurabilità.
    - modificato il recupero del GITHUB_TOKEN per differenziare tra ambiente di sviluppo e produzione, aumentando la sicurezza delle credenziali esposte.

--- src/shared/utils/env.ts
+++ src/shared/utils/env.ts
@@ -1,14 +1,16 @@
+const APP_ENV = import.meta.env.VITE_APP_ENV as 'development' | 'production'
+
 export const env = {
   // Configurazione server in locale
 VITE_API_URL: import.meta.env.VITE_API_URL,
 VITE_API_PORT: import.meta.env.VITE_API_PORT,
 
-// ! Questa e da cambiare urgente perche e esposta
-FORM_SPREE_ACTION_URL: import.meta.env.VITE_APP_FORM_SPREE_ACTION_URL,
-GITHUB_TOKEN: import.meta.env.VITE_GITHUB_TOKEN,
-
 // Altre configurazioni
 VITE_APP_ENV: import.meta.env.VITE_APP_ENV,
+
+// ! Questa e da cambiare urgente perche e esposta
+FORM_SPREE_ACTION_URL: import.meta.env.VITE_APP_FORM_SPREE_ACTION_URL,
+GITHUB_TOKEN: APP_ENV === 'development' ? import.meta.env.VITE_GITHUB_TOKEN : import.meta.env.GITHUB_TOKEN,
 
 // GitHub API endpoints
 GITHUB_API_BASE: import.meta.env.VITE_GITHUB_API_BASE,
