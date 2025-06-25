ca3ea17f5fbb8a1e0080bbc5f5643aac1e99d936
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-06-25 18:56:44 +0200

    feat: 🏷️ aggiungi il tipo Project per la gestione dei progetti
    - creato il nuovo tipo `Project` nel file `projects.ts` per definire la struttura dei dati dei progetti, inclusi nome, descrizione, tecnologie, data di creazione e URL delle immagini e README.

---
+++ src/shared/types/projects.ts
@@ -0,0 +1,9 @@
+export type Project = {
+  name: string;
+  description: string | null;
+  technologies: string[] | [];
+  createdAt: string;
+  imageUrl: string | null;
+  readmeUrl: string | null;
+  _v: string;
+};
