f51c1bc54a994c9dc4b4838884b1b1d36b5158fd
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-06-26 13:50:58 +0200

    refactor: ♻️ migliora la struttura delle variabili ambientali nel file env.ts
    - riorganizzate le variabili ambientali per una maggiore chiarezza e coerenza.
    - mantenuta la documentazione esistente per le configurazioni del server e gli endpoint GitHub.

--- src/shared/utils/env.ts
+++ src/shared/utils/env.ts
@@ -1,19 +1,17 @@
-
-
 export const env = {
   // Configurazione server in locale
-VITE_API_URL: import.meta.env.VITE_API_URL,
-VITE_API_PORT: import.meta.env.VITE_API_PORT,
+  VITE_API_URL: import.meta.env.VITE_API_URL,
+  VITE_API_PORT: import.meta.env.VITE_API_PORT,
 
-// Altre configurazioni
-VITE_APP_ENV: import.meta.env.VITE_APP_ENV,
+  // Altre configurazioni
+  VITE_APP_ENV: import.meta.env.VITE_APP_ENV,
 
-// ! Questa e da cambiare urgente perche e esposta
-FORM_SPREE_ACTION_URL: import.meta.env.VITE_APP_FORM_SPREE_ACTION_URL,
-GITHUB_TOKEN: import.meta.env.VITE_GITHUB_TOKEN,
+  // ! Questa e da cambiare urgente perche e esposta
+  FORM_SPREE_ACTION_URL: import.meta.env.VITE_APP_FORM_SPREE_ACTION_URL,
+  GITHUB_TOKEN: import.meta.env.VITE_GITHUB_TOKEN,
 
-// GitHub API endpoints
-GITHUB_API_BASE: import.meta.env.VITE_GITHUB_API_BASE,
-GITHUB_USERNAME: import.meta.env.VITE_GITHUB_USERNAME,
-GITHUB_REPO: import.meta.env.VITE_GITHUB_REPO,
-}
+  // GitHub API endpoints
+  GITHUB_API_BASE: import.meta.env.VITE_GITHUB_API_BASE,
+  GITHUB_USERNAME: import.meta.env.VITE_GITHUB_USERNAME,
+  GITHUB_REPO: import.meta.env.VITE_GITHUB_REPO,
+};
