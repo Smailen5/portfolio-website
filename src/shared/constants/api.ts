8f39442bbbc8d04dee4c84548d508f26af7f892f
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-06-26 13:40:35 +0200

    refactor: ♻️ aggiorna gli endpoint API per il progetto GitHub
    - rimosso l'utilizzo delle variabili ambientali per la costruzione degli endpoint.
    - aggiunto un endpoint statico per il recupero dei progetti da GitHub.
    - semplificata la configurazione degli endpoint API migliorando la chiarezza del codice.

--- src/shared/constants/api.ts
+++ src/shared/constants/api.ts
@@ -1,12 +1,5 @@
-import { env } from "@/shared/utils/env";
-
 export const API_ENDPOINTS = {
   GITHUB: {
-    REPO: `${env.GITHUB_API_BASE}/repos/${env.GITHUB_USERNAME}/${env.GITHUB_REPO}`,
-    CONTENTS: `${env.GITHUB_API_BASE}/repos/${env.GITHUB_USERNAME}/${env.GITHUB_REPO}/contents`,
-    PACKAGES: `${env.GITHUB_API_BASE}/repos/${env.GITHUB_USERNAME}/${env.GITHUB_REPO}/contents/packages`,
-    SCREENSHOTS: `${env.GITHUB_API_BASE}/repos/${env.GITHUB_USERNAME}/${env.GITHUB_REPO}/contents/screen-capture`,
-    README: (projectPath: string) =>
-      `${env.GITHUB_API_BASE}/repos/${env.GITHUB_USERNAME}/${env.GITHUB_REPO}/contents/${projectPath}/README.md`,
+    PROJECTS: "https://api.github.com/repos/Smailen5/Frontend-Mentor-Challenge/contents/public/projects.json",
   },
 };
