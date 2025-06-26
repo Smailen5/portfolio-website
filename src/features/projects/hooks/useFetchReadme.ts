86a0c1979ac4f97651d9da4cd3698a35e3c06ba2
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-06-26 14:19:37 +0200

    refactor: ♻️ aggiorna la funzione useFetchReadme per gestire il contenuto del README nella cache
    - aggiunto il parametro nome alla funzione useFetchReadme per migliorare la gestione del contenuto del README.
    - implementata la logica per aggiornare il contenuto del README nella sessionStorage se il progetto esiste già.

--- src/features/projects/hooks/useFetchReadme.ts
+++ src/features/projects/hooks/useFetchReadme.ts
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { projectService } from "../services/projectService";
 
-export const useFetchReadme = (readmeUrl: string | null) => {
+export const useFetchReadme = (readmeUrl: string | null, name: string | undefined) => {
   const [error, setError] = useState<boolean>(false);
   const [readmeContent, setReadmeContent] = useState<string | null>(null);
 
@@ -14,6 +14,15 @@ export const useFetchReadme = (readmeUrl: string | null) => {
       try {
         const response = await projectService.getReadme(readmeUrl);
         setReadmeContent(response.data);
+        
+        const cached = sessionStorage.getItem('projects')
+        if(cached) {
+          const parsed = JSON.parse(cached)
+
+          const updatedProjects = parsed.projects.map((project: any)=> project.name === name ? {...project, readmeContent: response.data} : project)
+
+          sessionStorage.setItem('projects', JSON.stringify({ projects: updatedProjects, timeStamp: parsed.timeStamp }))
+        }
       } catch (error) {
         setError(true);
       }
