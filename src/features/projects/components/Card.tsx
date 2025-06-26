d2543b8e1460af800e45d1a4771ce056001e9f18
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-06-26 12:20:13 +0200

    refactor: ♻️ aggiorna il componente CardProject per utilizzare il nuovo tipo Project
    - modificato il componente `CardProject` per accettare il tipo `Project`, semplificando la gestione delle proprietà.
    - rimosse le interfacce e le importazioni non utilizzate, migliorando la pulizia del codice.
    - aggiornato il rendering dell'immagine e delle informazioni del progetto per riflettere le nuove proprietà.

--- src/features/projects/components/Card.tsx
+++ src/features/projects/components/Card.tsx
@@ -2,42 +2,49 @@ import { ButtonsProject } from "@/features/projects/components/Buttons";
 import { ContainerProject } from "@/features/projects/components/Container";
 import { PictureProject } from "@/features/projects/components/Picture";
 import { useState } from "react";
-import Skeleton from "react-loading-skeleton";
+// import Skeleton from "react-loading-skeleton";
+import { Project } from "@/shared/types/projects";
 
-interface CardProjectType {
-  name: string;
-  html_url: string;
-  image: string;
-  technologies: string[];
-}
+// interface CardProjectType {
+//   name: string;
+//   html_url: string;
+//   image: string;
+//   technologies: string[];
+// }
 
-export const CardProject: React.FC<CardProjectType> = ({
+export const CardProject: React.FC<Project> = ({
   name,
-  html_url,
-  image,
+  description,
   technologies,
+  createdAt,
+  imageUrl,
+  readmeUrl,
+  version,
 }) => {
   const [imageLoad, setImgLoad] = useState(false);
   const [error, setError] = useState(false);
+
+  const nonUtilizzato = { description, createdAt, version }
+  console.log(nonUtilizzato)
 
   return (
     <>
       {/* PROGETTO SINGOLO */}
       <article className="flex flex-col gap-4 rounded-md border border-border bg-background text-foreground shadow-md">
         {/* Skeleton immagine */}
-        {!imageLoad && !error && <Skeleton height={250} />}
+        {/* {!imageLoad && !error && <Skeleton height={250} />} */}
 
         {/* immagine */}
         <PictureProject
-          linkSite={html_url}
-          image={image}
+          // linkSite={html_url}
+          image={imageUrl}
           name={name}
           onLoad={() => setImgLoad(true)}
           onError={() => setError(true)}
-          imageLoadFailed={error}
+          // imageLoadFailed={error}
         />
         {/* contenitore nome progetto e descrizione */}
-        <ContainerProject name={name} technologies={technologies}>
+        <ContainerProject name={name} technologies={technologies} description={description}>
           <ButtonsProject nome={name} />
         </ContainerProject>
       </article>
