0a6fe7e97f224c7672b3723d122f862be47a0c75
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-06-26 13:21:47 +0200

    refactor: ♻️ modifica la struttura dei tipi di progetto
    - introdotta l'interfaccia `CardProjectProps` per riutilizzare le proprietà comuni.
    - estesa l'interfaccia `Project` per includere `createdAt`, mantenendo la coerenza nella definizione dei tipi.
    - migliorata la leggibilità e la manutenibilità del codice.

--- src/shared/types/projects.ts
+++ src/shared/types/projects.ts
@@ -1,12 +1,15 @@
-export type Project = {
+export interface CardProjectProps {
   name: string;
   description: string | null;
   technologies: string[] | [];
-  createdAt: string;
   imageUrl: string | null;
+}
+
+export interface Project extends CardProjectProps {
+  createdAt: string;
   readmeUrl: string | null;
   version: string;
-};
+}
 
 export type CachedProjects = {
   projects: Project[];
