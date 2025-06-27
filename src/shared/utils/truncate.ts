666ded8755f750d3a7ce7255e487f46c183f073d
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-06-27 14:53:32 +0200

    feat: ✨ aggiunge funzione di troncamento del testo
    - creato il file truncate.ts con una funzione per troncare il testo se supera una lunghezza massima, migliorando la gestione del contenuto testuale nel progetto.

---
+++ src/shared/utils/truncate.ts
@@ -0,0 +1,4 @@
+// Tronca il testo se e' piu' lungo di maxLength
+export const truncate = (text: string, maxLength = 140) =>{
+  return text.length > maxLength ? text.slice(0, maxLength) + "..." : text
+}
