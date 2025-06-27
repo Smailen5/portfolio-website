20471a9f27778f7277af481f7fbe0ca658ab2832
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-06-27 15:58:35 +0200

    style: 💄 aggiorna il componente Header per includere un nuovo tipo 'span' e modifica le classi di dimensione del testo
    - aggiunto il tipo 'span' alle proprietà del componente Header e aggiornate le classi di dimensione del testo per i vari tipi, migliorando la coerenza visiva e la flessibilità del componente.

--- src/components/atoms/Header.tsx
+++ src/components/atoms/Header.tsx
@@ -5,22 +5,24 @@ interface HeaderProps {
   children: string;
   titleID?: string;
   className?: string;
-  type?: "h1" | "h2" | "h3" | "h4";
+  type?: "h1" | "h2" | "h3" | "h4" | 'span';
 }
 
-export const Header = ({ children, titleID, className, type="h1" }: HeaderProps) => {
+export const Header = ({ children, titleID, className, type }: HeaderProps) => {
     // Un po to much, ma sono stanco quindi va bene
     let textSize
     switch (type) {
-        case "h1": textSize ="text-3xl lg:text-4xl" 
+        case "h1": textSize ="text-2xl lg:text-3xl"
             break;
-        case "h2": textSize ="text-2xl lg:text-3xl";
+        case "h2": textSize ="text-xl lg:text-2xl";
             break;
-        case "h3": textSize ="text-xl lg:text-2xl";
+        case "h3": textSize ="text-lg lg:text-xl";
             break;
-        case "h4": textSize ="text-lg lg:text-xl";
+        case "h4": textSize ="text-base lg:text-lg";
             break;
-        default: textSize ="text-sm lg:text-lg"
+        default:
+          textSize ="text-sm lg:text-base";
+          type = "span";
             break;
     }
   return (
