60d66bf1cddde9e22de01771e50a7e25cd265efc
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-09-30 19:01:13 +0200

    feat: ✨ aggiunto componente Avatar nella Navbar
    - Inclusa l'istanza del componente Avatar all'interno della Navbar per visualizzare l'utente corrente.
    - Aggiornato il layout della Navbar per integrare il nuovo componente.

--- src/components/organisms/Navbar/Navbar.tsx
+++ src/components/organisms/Navbar/Navbar.tsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import { DesktopNavbar } from "./DesktopNavbar";
 import { SideBar } from "./SideBar";
+import { Avatar } from '@/components/atoms/Avatar';
 
 export const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <>
       <nav className="flex items-center justify-between bg-red-500 px-4 py-2">
-        <div>logo</div>
+        <Avatar name='Smailen Vargas'/>
 
         {/* Menu Mobile: Bottone menu + SideBar */}
         <div className="lg:hidden">
