bc55c1d8c6217deea816cc1ad3d8003598fe3ea1
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-09-30 16:51:51 +0200

    feat: ✨ aggiunta funzionalità di apertura del menu nella Navbar
    - Implementato uno stato per gestire l'apertura del SideBar.
    - Aggiunto un pulsante "Menu" per attivare il SideBar nella Navbar.

--- src/components/organisms/Navbar/Navbar.tsx
+++ src/components/organisms/Navbar/Navbar.tsx
@@ -1,8 +1,17 @@
+import { useState } from 'react';
+import { SideBar } from './SideBar';
+
 export const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <nav className='bg-red-500'>
+    <>
+    <nav className="flex items-center justify-between bg-red-500 px-4 py-2">
       <div>logo</div>
-      <div>Navbar</div>
+      <div>
+        <button onClick={()=> setIsOpen(true)}>Menu</button>
+      </div>
     </nav>
+    <SideBar isOpen={isOpen} setIsOpen={setIsOpen} />
+    </>
   );
 };
