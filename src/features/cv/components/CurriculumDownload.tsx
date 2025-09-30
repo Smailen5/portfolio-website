6cd22c586384a983654893832102c8c959023fad
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-09-30 18:06:52 +0200

    feat: ✨ aggiunto componente CurriculumDownload per il download del CV
    - Creato il componente CurriculumDownload con opzioni per scaricare il CV in italiano e inglese.
    - Implementata la logica per mostrare/nascondere le opzioni di download e gestire il click sul pulsante.

---
+++ src/features/cv/components/CurriculumDownload.tsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+
+// Props per il componente CurriculumDownload
+interface CurriculumDownloadProps {
+  closeSideBar: () => void;
+}
+
+export const CurriculumDownload = ({
+  closeSideBar,
+}: CurriculumDownloadProps) => {
+  const [showOptions, setShowOptions] = useState(false);
+  // Handler per il click sul download
+  const handleDownload = () => {
+    setShowOptions(false);
+    closeSideBar();
+  };
+
+  return (
+    <div className="relative">
+      {/* Pulsante per mostrare le opzioni CV */}
+      <button
+        onClick={() => {
+          setShowOptions(!showOptions);
+        }}
+        className="text-base font-semibold"
+      >
+        Curriculum
+      </button>
+
+      {/* Opzioni di download CV */}
+
+      <div
+        className={`absolute left-0 -right-10 mt-2 shadow-lg transition-transform duration-300 ease-in-out ${
+          showOptions ? "translate-x-0" : "translate-x-full"
+        }`}
+      >
+        <a
+          href="/assets/curriculum/CV-Smailen-Vargas-Frontend-IT.pdf"
+          download="CV-Smailen-Vargas-Frontend-IT.pdf"
+          className="block w-full p-3 text-left transition-colors hover:bg-gray-100"
+          onClick={handleDownload}
+        >
+          Curriculum in Italiano
+        </a>
+        <a
+          href="/assets/curriculum/CV-Smailen-Vargas-Frontend-EN.pdf"
+          download="CV-Smailen-Vargas-Frontend-EN.pdf"
+          className="block w-full p-3 text-left transition-colors hover:bg-gray-100"
+          onClick={handleDownload}
+        >
+          Curriculum in Inglese
+        </a>
+      </div>
+    </div>
+  );
+};
