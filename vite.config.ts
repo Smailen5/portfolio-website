457fe8a6d48f7798d0cafc4f8bda5842aaf00d75
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-09-30 16:09:08 +0200

    chore: aggiunta integrazione di Tailwind CSS nel progetto
    - Aggiunti i pacchetti "@tailwindcss/vite" e "tailwindcss" in package.json.
    - Aggiornato il file vite.config.ts per includere il plugin Tailwind CSS.
    - Creato il file app.css per importare Tailwind CSS.

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import path from "path";
@@ -6,7 +7,7 @@ import tsConfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tsConfigPaths(), tanstackStart(), viteReact()],
+  plugins: [tsConfigPaths(), tanstackStart(), viteReact(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
