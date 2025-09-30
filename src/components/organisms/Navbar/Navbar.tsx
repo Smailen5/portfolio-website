75068e6d24b5d93cdf93920b193c1cd5e15bc027
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-09-30 16:52:41 +0200

    refactor: ♻️ migliorata la formattazione del codice nella Navbar e SideBar
    - Uniformato l'uso delle virgolette nel file Navbar.tsx.
    - Ristrutturato il codice nel file SideBar.tsx per una maggiore leggibilità, separando le proprietà in righe distinte.

--- src/components/organisms/Navbar/Navbar.tsx
+++ src/components/organisms/Navbar/Navbar.tsx
@@ -1,17 +1,17 @@
-import { useState } from 'react';
-import { SideBar } from './SideBar';
+import { useState } from "react";
+import { SideBar } from "./SideBar";
 
 export const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <>
-    <nav className="flex items-center justify-between bg-red-500 px-4 py-2">
-      <div>logo</div>
-      <div>
-        <button onClick={()=> setIsOpen(true)}>Menu</button>
-      </div>
-    </nav>
-    <SideBar isOpen={isOpen} setIsOpen={setIsOpen} />
+      <nav className="flex items-center justify-between bg-red-500 px-4 py-2">
+        <div>logo</div>
+        <div>
+          <button onClick={() => setIsOpen(true)}>Menu</button>
+        </div>
+      </nav>
+      <SideBar isOpen={isOpen} setIsOpen={setIsOpen} />
     </>
   );
 };
