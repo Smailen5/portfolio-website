e19eeaa9b11519b312a2a8a0535b0bb917268013
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-09-30 13:20:11 +0200

    chore: 🛠️ integrato il router nel file main.tsx
    - Sostituito il componente App con RouterProvider per gestire le rotte utilizzando il nuovo router creato con @tanstack/react-router.
    - Aggiunti log per il router e la sua struttura ad albero.

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,21 @@
-import App from "@/App.tsx";
 import { AppProvider } from "@/shared/utils/context";
 import { ThemeProvider } from "@/shared/utils/themeProvider";
+import { RouterProvider } from "@tanstack/react-router";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { getRouter } from "./router";
+
+const router = getRouter();
+
+console.log("Router created:", router);
+console.log("Router tree:", router.routeTree);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AppProvider>
       <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <App />
+        <RouterProvider router={router} />
       </ThemeProvider>
     </AppProvider>
   </StrictMode>,
