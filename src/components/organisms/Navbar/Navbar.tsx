a626384fdfe9b7cb9f2b6f7dbd01879ff275ca4f
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-09-30 18:32:49 +0200

    feat: ✨ aggiornato il componente Navbar per supportare la navigazione desktop e mobile
    - Aggiunto il componente DesktopNavbar per la navigazione su schermi grandi.
    - Ristrutturato il layout della Navbar per includere il SideBar per la navigazione mobile.
    - Ottimizzato il codice per una migliore leggibilità e gestione degli stati.

--- src/components/organisms/Navbar/Navbar.tsx
+++ src/components/organisms/Navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { SideBar } from "./SideBar";
+import { DesktopNavbar } from '../DesktopNavbar';
 
 export const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -7,11 +8,19 @@ export const Navbar = () => {
     <>
       <nav className="flex items-center justify-between bg-red-500 px-4 py-2">
         <div>logo</div>
-        <div>
+
+        {/* Menu Mobile: Bottone menu + SideBar */}
+        <div className="lg:hidden">
           <button onClick={() => setIsOpen(true)}>Menu</button>
+
+          <SideBar isOpen={isOpen} setIsOpen={setIsOpen} />
+        </div>
+
+        {/* Menu Desktop */}
+        <div className='hidden lg:block'>
+          <DesktopNavbar />
         </div>
       </nav>
-      <SideBar isOpen={isOpen} setIsOpen={setIsOpen} />
     </>
   );
 };
