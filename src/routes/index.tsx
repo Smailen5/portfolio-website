e854c2f973b3a1b39e5d3d33be341c6de0756e8d
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-09-30 13:48:33 +0200

    fix: 🛠️ aggiunta la HomePage come componente della rotta principale
    - Importato il componente HomePage e associato alla rotta principale utilizzando TanStack Router.
    - Rimosso il codice commentato relativo alla funzione HomePage per una maggiore chiarezza.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,14 @@
 // src/routes/index.tsx
 
 import { createFileRoute } from "@tanstack/react-router";
+import HomePage from "@pages/Homepage";
 
-export const Route = createFileRoute("/")()
+export const Route = createFileRoute("/")({
+  component: HomePage,
+});
+
+// function HomePage() {
+//   console.log("HomePage rendering");
+
+//   return <div>Hello tanstack</div>;
+// }
