7ddb3f4da154bfebc2d7558c5332919ac5f465c1
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-10-02 15:58:26 +0200

    refactor: ♻️ commentato il componente HomePage e aggiornato l'importazione
    - Commentato il codice del componente HomePage per una gestione più chiara.
    - Aggiornata l'importazione del componente per riflettere le modifiche nel file.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,14 @@
 // src/routes/index.tsx
 
 import { createFileRoute } from "@tanstack/react-router";
-// import HomePage from "@pages/Homepage";
+import HomePage from "@pages/Homepage";
 
 export const Route = createFileRoute("/")({
   component: HomePage,
 });
 
-function HomePage() {
-  console.log("HomePage rendering");
+// function HomePage() {
+//   console.log("HomePage rendering");
 
-  return <div>Hello tanstack home page</div>;
-}
+//   return <div>Hello tanstack home page</div>;
+// }
