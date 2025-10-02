103e9d47b2e9221765e420eaa2994bc506a1c9d2
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-10-02 13:35:07 +0200

    style: 💄 migliorato lo styling della SideBar
    - Modificato l'ordine delle classi CSS per una migliore leggibilità e coerenza.
    - Aggiornato il contenitore principale della SideBar per riflettere le modifiche stilistiche.

--- src/components/organisms/Navbar/SideBar.tsx
+++ src/components/organisms/Navbar/SideBar.tsx
@@ -16,18 +16,18 @@ export const SideBar = ({
     <>
       {/* Overlay scuro che copre la pagina quando SideBar è aperto */}
       <div
-        className={`fixed inset-0 z-10 h-screen bg-base-300 transition-opacity duration-300 lg:hidden ${isOpen ? "opacity-80" : "pointer-events-none opacity-0"} `}
+        className={`bg-base-300 fixed inset-0 z-10 h-screen transition-opacity duration-300 lg:hidden ${isOpen ? "opacity-80" : "pointer-events-none opacity-0"} `}
         onClick={closeSideBar}
       />
 
       {/* Contenitore principale della SideBar */}
       <div
-        className={`fixed inset-y-0 top-0 right-0 z-20 h-screen w-[70%] transform bg-base-200 shadow-lg transition-transform duration-300 ease-in-out lg:hidden ${
+        className={`bg-base-200 fixed inset-y-0 top-0 right-0 z-20 h-screen w-[70%] transform shadow-lg transition-transform duration-300 ease-in-out lg:hidden ${
           isOpen ? "translate-x-0" : "translate-x-full"
         }`}
       >
         <div className="p-4">
-          <div className='flex items-center justify-between pb-6'>
+          <div className="flex items-center justify-between pb-6">
             <button onClick={() => setIsOpen(false)}>chiudi</button>
           </div>
 
