89e9035b47abb8b4db3676bb444891216e735294
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-10-02 15:55:31 +0200

    refactor: ♻️ spostato il componente Layout nella nuova posizione
    - Rimosso il file Layout.tsx dalla cartella backup-components e creato un nuovo file Layout.tsx nella cartella src/components/molecules.
    - Ottimizzato il layout mantenendo la funzionalità esistente.

---
+++ src/components/molecules/Layout.tsx
@@ -0,0 +1,14 @@
+import useScroll from "@/shared/hooks/useScroll";
+
+export const Layout = ({ children }: { children: React.ReactNode }) => {
+  useScroll();
+  return (
+    <>
+      <div className="flex h-auto w-full justify-center bg-background">
+        <div className="flex w-full max-w-[1024px] flex-col flex-nowrap gap-20 px-6 pb-14 pt-36 md:items-center">
+          {children}
+        </div>
+      </div>
+    </>
+  );
+};
