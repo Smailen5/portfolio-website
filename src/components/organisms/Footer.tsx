b0488ce47d1848b5444286bae1e3c93dea97059f
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-10-02 18:19:04 +0200

    style: 💄 migliorato il layout del footer e aggiornato il link a Icons8
    - Riorganizzato le classi CSS nel componente Footer per una migliore leggibilità e coerenza.
    - Sostituito il componente LinkBlank con un tag <a> per il link a Icons8, migliorando l'accessibilità e la compatibilità.

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -1,25 +1,30 @@
 import reactIcon from "@/assets/icons/icons8-react.svg";
 import tailwindIcon from "@/assets/icons/icons8-tailwind-css.svg";
 import SocialIcons from "@/components/molecules/Social/SocialIcon";
-import { LinkBlank } from "@components/atoms/LinkBlank";
 
 const Footer = () => {
   return (
-    <footer className="flex w-full flex-row items-center justify-center bg-background text-center">
+    <footer className="bg-background flex w-full flex-row items-center justify-center text-center">
       <div className="flex w-full max-w-[1024px] flex-col gap-4 px-6 py-4 md:flex-row md:justify-between">
         <div className="flex justify-center gap-4 md:order-2">
           <p className="hidden md:block">Powered by</p>
           <img src={tailwindIcon} alt="icona tailwind css" className="size-6" />
           <img src={reactIcon} alt="icona react" className="size-6" />
         </div>
-        <div className="space-y-4 flex flex-col items-start">
+        <div className="flex flex-col items-start space-y-4">
           <p>
-            © 2024-{new Date().getFullYear()} Smailen Vargas. Tutti i diritti riservati.{" "}
+            © 2024-{new Date().getFullYear()} Smailen Vargas. Tutti i diritti
+            riservati.{" "}
             <span>
               Icons by{" "}
-              <LinkBlank href="https://icons8.com" isText>
+              <a
+                href={"https://icons8.com"}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-accent font-bold"
+              >
                 Icons8
-              </LinkBlank>
+              </a>
             </span>
           </p>
           <SocialIcons />
