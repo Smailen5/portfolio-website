d050e6f2cfb00ad6530640149001283253354676
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-10-03 16:40:32 +0200

    refactor: ♻️ semplificato il componente SectionProjects
    - Rimosso codice commentato e hook non utilizzati, migliorando la leggibilità del componente.
    - Aggiornato il rendering per visualizzare i progetti recuperati senza l'uso del filtro, semplificando la logica di presentazione.

--- src/features/projects/components/Section.tsx
+++ src/features/projects/components/Section.tsx
@@ -1,8 +1,3 @@
-// import { ErrorMessage } from "@/components/atoms/ErrorMessage";
-// import { CardProject } from "@/features/projects/components/Card";
-// import { SkeletonLoaderCard } from "@/features/projects/components/Skeleton";
-// import { useFilter } from "@/features/projects/hooks/useFilter";
-// import { Filter } from "@features/projects/components/Filter";
 
 import { fetchProjects } from '@/server/getProjects';
 import { Project } from '@/shared/types/projects';
@@ -12,13 +7,6 @@ import { CardProject } from './Card';
 import { Filter } from './Filter';
 
 export const SectionProjects = () => {
-  // const {
-  //   loading,
-  //   error,
-  //   filteredProjects,
-  //   setSelectedFilter,
-  //   numberFilteredProjects,
-  // } = useFilter();
   const getProjects = useServerFn(fetchProjects);
 
   const [projects, setProjects] = useState<Project[]>([]);
@@ -35,25 +23,6 @@ export const SectionProjects = () => {
 
   return (
     <>
-      {/* <Filter
-        setSelectedFilter={setSelectedFilter}
-        number={numberFilteredProjects}
-      />
-      {numberFilteredProjects === 0 ? (
-        <p>Nessun progetto trovato.</p>
-      ) : error ? (
-        <ErrorMessage>{error}</ErrorMessage>
-      ) : (
-        <section className="grid w-full grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-          {loading
-            ? filteredProjects.map((_, index) => (
-                <SkeletonLoaderCard key={index} />
-              ))
-            : filteredProjects.map((project, index) => {
-                return <CardProject key={index} {...project} />;
-              })}
-        </section>
-      )} */}
       <div>
         <Filter number={filteredProjects.length} setFilteredProject={setFilteredProjects} projectsNoFiltered={projects} />
         <section className='grid w-full grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3'>
