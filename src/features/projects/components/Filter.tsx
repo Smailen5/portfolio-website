d1d7ad6588eae5f6b822842a83416ae15b43c3d5
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-10-03 16:39:52 +0200

    refactor: ♻️ aggiornato il componente Filter e rimosso FilterSelect
    - Modificato il componente Filter per gestire il filtraggio dei progetti in base alla tecnologia selezionata, utilizzando un array di tecnologie.
    - Rimosso il componente FilterSelect, ora non più necessario, semplificando la logica di filtraggio.
    - Aggiornato il componente SectionProjects per integrare il nuovo sistema di filtraggio e visualizzare i progetti filtrati.

--- src/features/projects/components/Filter.tsx
+++ src/features/projects/components/Filter.tsx
@@ -1,15 +1,53 @@
-import { FilterSelect } from "@/features/projects/components/FilterSelect";
-import { Dispatch, SetStateAction } from "react";
+import { Project } from '@/shared/types/projects';
+import { Dispatch, SetStateAction, useEffect } from 'react';
 
 interface FilterProps {
-  setSelectedFilter: Dispatch<SetStateAction<string>>;
-  number: number;
+  setFilteredProject: Dispatch<SetStateAction<Project[]>>;
+  projectsNoFiltered: Project[];
+  number?: number;
 }
 
-export const Filter = ({ setSelectedFilter, number }: FilterProps) => {
+const technologies = [
+  "Tutto",
+  "JavaScript",
+  "TypeScript",
+  "Tailwind CSS",
+  "React",
+  "Next.js",
+  "Form",
+  "Responsive",
+]
+
+export const Filter = ({ setFilteredProject, projectsNoFiltered,number }: FilterProps) => {
+
+  useEffect(()=>{
+    setFilteredProject(projectsNoFiltered)
+
+  },[projectsNoFiltered, setFilteredProject])
+
+  const handleFilter = (technology: string) => {
+    if(technology === "Tutto") return setFilteredProject(projectsNoFiltered)
+
+    const filteredProjects = projectsNoFiltered.filter((project) => {
+
+      // Se non ci sono tecnologie, non filtrare
+      if(!project.technologies || project.technologies.length === 0 ) return false
+
+      return project.technologies.includes(technology.toLowerCase())
+    })
+    setFilteredProject(filteredProjects)
+  }
+
   return (
-    <section className="flex w-full flex-col items-start gap-8 rounded-md bg-secondary p-2 text-secondary-foreground md:flex-row md:items-end md:justify-between md:gap-0">
-      <FilterSelect onSelectionChange={setSelectedFilter} />
+    <section className="bg-secondary text-secondary-foreground flex w-full flex-col items-start gap-8 rounded-md p-2 md:flex-row md:items-end md:justify-between md:gap-0">
+      <details className='dropdown'>
+        <summary className='btn m-1'>Seleziona una tecnologia</summary>
+        <ul className='menu dropdown-content bg-base-100 rounded-box z-10 w-52 p-2 shadow-sm'>
+          {technologies.map((technologie)=>{
+            return <li key={technologie}><a onClick={()=> handleFilter(technologie)}>{technologie}</a></li>
+          })}
+        </ul>
+      </details>
       <p>
         Progetti visualizzati: <span className="font-semibold">{number}</span>
       </p>
--- src/features/projects/components/Section.tsx
+++ src/features/projects/components/Section.tsx
@@ -9,6 +9,7 @@ import { Project } from '@/shared/types/projects';
 import { useServerFn } from '@tanstack/react-start';
 import { useEffect, useState } from 'react';
 import { CardProject } from './Card';
+import { Filter } from './Filter';
 
 export const SectionProjects = () => {
   // const {
@@ -21,6 +22,7 @@ export const SectionProjects = () => {
   const getProjects = useServerFn(fetchProjects);
 
   const [projects, setProjects] = useState<Project[]>([]);
+  const [filteredProjects, setFilteredProjects] = useState<Project[]>([]);
 
   useEffect(()=> {
     getProjects().then(data => {
@@ -53,8 +55,9 @@ export const SectionProjects = () => {
         </section>
       )} */}
       <div>
+        <Filter number={filteredProjects.length} setFilteredProject={setFilteredProjects} projectsNoFiltered={projects} />
         <section className='grid w-full grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3'>
-        {projects.map((project : Project) => (
+        {filteredProjects.map((project : Project) => (
           <CardProject key={project.name} {...project} />
         )) }
         </section>
