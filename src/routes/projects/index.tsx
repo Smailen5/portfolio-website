c8fd965a69869cf77d4230f7ba669feda91d333a
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-10-03 14:41:59 +0200

    refactor: ♻️ aggiornata la route per i progetti
    - Aggiunto un loader per fetchare i progetti tramite il nuovo servizio fetchProjects, migliorando l'efficienza del caricamento dei dati nella route "/projects/".

--- src/routes/projects/index.tsx
+++ src/routes/projects/index.tsx
@@ -1,6 +1,8 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { fetchProjects } from "@/server/getProjects";
 import ProjectPage from "@pages/ProjectPage";
+import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/projects/")({
+  loader: () => fetchProjects(),
   component: ProjectPage,
 });
