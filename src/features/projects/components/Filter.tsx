a4fa3c485ed33e4a388c2d59397dd7b2f53b8a6a
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-10-03 16:40:48 +0200

    refactor: ♻️ migliorata la leggibilità del componente Filter
    - Uniformato l'uso delle virgolette nel codice.
    - Ristrutturato il codice per migliorare la chiarezza e la coerenza, mantenendo la funzionalità di filtraggio dei progetti in base alla tecnologia selezionata.

--- src/features/projects/components/Filter.tsx
+++ src/features/projects/components/Filter.tsx
@@ -1,5 +1,5 @@
-import { Project } from '@/shared/types/projects';
-import { Dispatch, SetStateAction, useEffect } from 'react';
+import { Project } from "@/shared/types/projects";
+import { Dispatch, SetStateAction, useEffect } from "react";
 
 interface FilterProps {
   setFilteredProject: Dispatch<SetStateAction<Project[]>>;
@@ -16,35 +16,41 @@ const technologies = [
   "Next.js",
   "Form",
   "Responsive",
-]
+];
 
-export const Filter = ({ setFilteredProject, projectsNoFiltered,number }: FilterProps) => {
-
-  useEffect(()=>{
-    setFilteredProject(projectsNoFiltered)
-
-  },[projectsNoFiltered, setFilteredProject])
+export const Filter = ({
+  setFilteredProject,
+  projectsNoFiltered,
+  number,
+}: FilterProps) => {
+  useEffect(() => {
+    setFilteredProject(projectsNoFiltered);
+  }, [projectsNoFiltered, setFilteredProject]);
 
   const handleFilter = (technology: string) => {
-    if(technology === "Tutto") return setFilteredProject(projectsNoFiltered)
+    if (technology === "Tutto") return setFilteredProject(projectsNoFiltered);
 
     const filteredProjects = projectsNoFiltered.filter((project) => {
-
       // Se non ci sono tecnologie, non filtrare
-      if(!project.technologies || project.technologies.length === 0 ) return false
+      if (!project.technologies || project.technologies.length === 0)
+        return false;
 
-      return project.technologies.includes(technology.toLowerCase())
-    })
-    setFilteredProject(filteredProjects)
-  }
+      return project.technologies.includes(technology.toLowerCase());
+    });
+    setFilteredProject(filteredProjects);
+  };
 
   return (
     <section className="bg-secondary text-secondary-foreground flex w-full flex-col items-start gap-8 rounded-md p-2 md:flex-row md:items-end md:justify-between md:gap-0">
-      <details className='dropdown'>
-        <summary className='btn m-1'>Seleziona una tecnologia</summary>
-        <ul className='menu dropdown-content bg-base-100 rounded-box z-10 w-52 p-2 shadow-sm'>
-          {technologies.map((technologie)=>{
-            return <li key={technologie}><a onClick={()=> handleFilter(technologie)}>{technologie}</a></li>
+      <details className="dropdown">
+        <summary className="btn m-1">Seleziona una tecnologia</summary>
+        <ul className="menu dropdown-content bg-base-100 rounded-box z-10 w-52 p-2 shadow-sm">
+          {technologies.map((technologie) => {
+            return (
+              <li key={technologie}>
+                <a onClick={() => handleFilter(technologie)}>{technologie}</a>
+              </li>
+            );
           })}
         </ul>
       </details>
