c1a9190a30d6b5a1ab4cb19fcb8f0e7ef832859e
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-10-10 14:06:39 +0200

    refactor: ♻️ rimosso l'uso di react-helmet e integrato il meta tag direttamente nella definizione della route
    - Semplificata la gestione dei meta tag per la pagina dei progetti, migliorando la coerenza del codice e riducendo le dipendenze.

--- src/routes/projects/index.tsx
+++ src/routes/projects/index.tsx
@@ -3,32 +3,34 @@ import { Layout } from "@/components/molecules/Layout";
 import { HeaderProject } from "@/features/projects/components/Header";
 import { SectionProjects } from "@/features/projects/components/Section";
 import { createFileRoute, useLoaderData } from "@tanstack/react-router";
-import { Helmet } from "react-helmet";
 
 export const Route = createFileRoute("/projects/")({
   loader: () => fetchProjects(),
   component: ProjectPage,
+  head: () => ({
+    meta: [
+      {
+        title: "Progetti di Smailen Vargas | Frontend Developer",
+      },
+      {
+        name: "description",
+        content:
+          "Esplora i progetti realizzati da Smailen Vargas come frontend developer",
+      },
+      {
+        name: "keywords",
+        content: "Smailen Vargas, Progetti, Frontend Developer",
+      },
+    ],
+  }),
 });
 
 function ProjectPage() {
   const projects = useLoaderData({ from: "/projects/" });
   return (
-    <>
-      <Helmet>
-        <title>Progetti di Smailen Vargas | Frontend Developer</title>
-        <meta
-          name="description"
-          content="Esplora i progetti realizzati da Smailen Vargas come frontend developer"
-        />
-        <meta
-          name="keywords"
-          content="Smailen Vargas, Progetti, Frontend Developer"
-        />
-      </Helmet>
-      <Layout>
-        <HeaderProject />
-        <SectionProjects projects={projects} />
-      </Layout>
-    </>
+    <Layout>
+      <HeaderProject />
+      <SectionProjects projects={projects} />
+    </Layout>
   );
 }
