d8592fbdd64a90e60a1f9a4239a1b60f999ac207
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-10-10 15:40:33 +0200

    chore: 🔧 aggiornati i meta tag nel componente Head per migliorare SEO
    - Aggiunti nuovi meta tag per Open Graph e Mobile App, migliorando la condivisione sui social media e l'esperienza utente.
    - Integrato JSON-LD per dati strutturati, arricchendo le informazioni SEO e la visibilità sui motori di ricerca.

--- src/shared/utils/Head.tsx
+++ src/shared/utils/Head.tsx
@@ -1,13 +1,17 @@
 // src/shared/utils/Head.tsx
 
-import { SITE_INFO } from "@/shared/constants/metaTags";
+import {
+  JSON_LD_PERSON,
+  MOBILE_APP_TAGS,
+  OPEN_GRAPH_TAGS,
+  SITE_INFO,
+} from "@/shared/constants/metaTags";
 import { Helmet } from "react-helmet";
 
 interface HeadProps {
   title?: string;
   description?: string;
   keywords?: string;
-  ogImage?: string;
   ogUrl?: string;
 }
 
@@ -19,7 +23,6 @@ export const Head = ({
   title = "Smailen Vargas | Frontend Developer",
   description = "Ciao, sono Smailen Vargas, Frontend Developer specializzato in React, TypeScript e Tailwind CSS",
   keywords = "Smailen Vargas, Frontend Developer, React, TypeScript, Tailwind CSS, DaisyUi, Portfolio",
-  ogImage = `${SITE_INFO.url}/images/immagine-sito.jpeg`,
   ogUrl = SITE_INFO.url,
 }: HeadProps) => {
   return (
@@ -31,16 +34,24 @@ export const Head = ({
       <meta name="author" content={SITE_INFO.author} />
 
       {/* Open Graph */}
-      <meta property="og:type" content="website" />
-      <meta property="og:locale" content={SITE_INFO.locale} />
-      <meta property="og:site_name" content={SITE_INFO.name} />
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
-      <meta property="og:image" content={ogImage} />
+      {OPEN_GRAPH_TAGS.map((tag) => (
+        <meta
+          key={tag.property}
+          property={tag.property}
+          content={tag.content}
+        />
+      ))}
       <meta property="og:url" content={ogUrl} />
 
       {/* Mobile App */}
-      <meta name="theme-color" content={SITE_INFO.themeColor} />
+      {MOBILE_APP_TAGS.map((tag) => (
+        <meta key={tag.name} name={tag.name} content={tag.content} />
+      ))}
+
+      {/* JSON-LD Structured Data */}
+      <script type="application/ld+json">
+        {JSON.stringify(JSON_LD_PERSON)}
+      </script>
     </Helmet>
   );
 };
