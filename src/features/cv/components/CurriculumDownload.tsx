9ea4afc7bb8843ee1633a0dc97040b2a4e7b798b
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-10-13 15:22:55 +0200

    refactor: ♻️ semplificato il componente CurriculumDownload per migliorare la leggibilità
    - Rimosso lo stato locale per la gestione delle opzioni di download.
    - Sostituito il markup con un elemento `<details>` per una presentazione più chiara delle opzioni di download.
    - Ottimizzato il codice per mantenere la funzionalità senza compromettere l'estetica.

--- src/features/cv/components/CurriculumDownload.tsx
+++ src/features/cv/components/CurriculumDownload.tsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 // Props per il componente CurriculumDownload
 interface CurriculumDownloadProps {
   closeSideBar?: () => void;
@@ -8,49 +6,37 @@ interface CurriculumDownloadProps {
 export const CurriculumDownload = ({
   closeSideBar,
 }: CurriculumDownloadProps) => {
-  const [showOptions, setShowOptions] = useState(false);
   // Handler per il click sul download
   const handleDownload = () => {
-    setShowOptions(false);
     closeSideBar?.();
   };
 
   return (
-    <div className="relative">
-      {/* Pulsante per mostrare le opzioni CV */}
-      <button
-        onClick={() => {
-          setShowOptions(!showOptions);
-        }}
-        className="text-base font-semibold"
-      >
-        Curriculum
-      </button>
+    <>
+      <details className="dropdown">
+        <summary className="m-1 text-base font-semibold">Curriculum</summary>
 
-      {/* Opzioni di download CV */}
-
-      <div
-        className={`absolute left-0 -right-10 mt-2 shadow-lg bg-base-100 transition-transform duration-300 ease-in-out ${
-          showOptions ? "translate-x-0" : "translate-x-full"
-        }`}
-      >
-        <a
-          href="/assets/curriculum/CV-Smailen-Vargas-Frontend-IT.pdf"
-          download="CV-Smailen-Vargas-Frontend-IT.pdf"
-          className="block w-full p-3 text-left transition-colors hover:bg-gray-100"
-          onClick={handleDownload}
-        >
-          Curriculum in Italiano
-        </a>
-        <a
-          href="/assets/curriculum/CV-Smailen-Vargas-Frontend-EN.pdf"
-          download="CV-Smailen-Vargas-Frontend-EN.pdf"
-          className="block w-full p-3 text-left transition-colors hover:bg-gray-100"
-          onClick={handleDownload}
-        >
-          Curriculum in Inglese
-        </a>
-      </div>
-    </div>
+        <ul className="menu dropdown-content bg-base-100 rounded-box z-10 w-52 p-2 shadow-sm">
+          <li>
+            <a
+              href="/assets/curriculum/CV-Smailen-Vargas-Frontend-IT.pdf"
+              download="CV-Smailen-Vargas-Frontend-IT.pdf"
+              onClick={handleDownload}
+            >
+              Curriculum in Italiano
+            </a>
+          </li>
+          <li>
+            <a
+              href="/assets/curriculum/CV-Smailen-Vargas-Frontend-EN.pdf"
+              download="CV-Smailen-Vargas-Frontend-EN.pdf"
+              onClick={handleDownload}
+            >
+              Curriculum in Inglese
+            </a>
+          </li>
+        </ul>
+      </details>
+    </>
   );
 };
