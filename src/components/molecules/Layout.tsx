324a384f41647a06e1123e889e4131507c49bb1d
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-10-13 14:49:56 +0200

    refactor: ♻️ semplificato il layout del componente Layout rimuovendo classi non necessarie
    - Eliminata la classe `bg-background` dal div principale per migliorare la chiarezza del markup.
    - Aggiornato il layout per mantenere la funzionalità senza compromettere l'estetica.

--- src/components/molecules/Layout.tsx
+++ src/components/molecules/Layout.tsx
@@ -4,7 +4,7 @@ export const Layout = ({ children }: { children: React.ReactNode }) => {
   useScroll();
   return (
     <>
-      <div className="flex h-auto w-full justify-center bg-background">
+      <div className="flex h-auto w-full justify-center">
         <div className="flex w-full max-w-[1024px] flex-col flex-nowrap gap-20 px-6 pb-14 pt-36 md:items-center">
           {children}
         </div>
