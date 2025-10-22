1b7a7555f477d75ebf12fe8e28744f113128deb9
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-10-22 12:02:54 +0200

    refactor: 💄 migliora la struttura del layout e la leggibilità del contenuto nella pagina di contatto
    Riorganizzato il layout per una migliore allineamento e accessibilità, mantenendo il contenuto centrale e semplificando la presentazione delle informazioni di contatto.

--- src/routes/contact/index.tsx
+++ src/routes/contact/index.tsx
@@ -18,48 +18,50 @@ function ContactPage() {
         keywords="Contatti, Smailen Vargas, Email, Collaborazioni, Frontend Developer"
         ogUrl="https://smailenvargas.com/contact"
       />
-      <Layout classContent="p-6 md:h-[calc(100vh-160px)] md:text-center">
-        <section
-          aria-labelledby="contact-heading"
-          className="flex-1 space-y-4 md:flex md:flex-col md:justify-center lg:w-1/2"
-        >
-          <h3 className="sr-only">Contattami</h3>
-          <p>
-            Se hai domande, richieste di collaborazione, o semplicemente vuoi
-            parlare di React, non esitare a contattarmi. Sarò felice di
-            risponderti il prima possibile!
-          </p>
-        </section>
-
-        <div className="flex flex-col gap-8 pt-10">
-          {/* Email */}
-          <div className="flex flex-col gap-2">
+      <Layout classContent="p-6 h-[calc(100vh-160px)] md:text-center">
+        <div className="flex h-full flex-col items-center justify-center">
+          <section
+            aria-labelledby="contact-heading"
+            className="space-y-4 lg:w-1/2"
+          >
+            <h3 className="sr-only">Contattami</h3>
             <p>
-              Scrivimi a:{' '}
-              <a
-                href="mailto:smailenv91@gmail.com?subject=42-website-contact"
-                className="link link-accent font-semibold"
-              >
-                smailenv91@gmail.com
-              </a>
+              Se hai domande, richieste di collaborazione, o semplicemente vuoi
+              parlare di React, non esitare a contattarmi. Sarò felice di
+              risponderti il prima possibile!
             </p>
+          </section>
 
-            <p className="text-xs">
-              (Clicca per aprire il tuo client email, oppure copia
-              l&apos;indirizzo)
-            </p>
+          <div className="flex flex-col gap-8 pt-10">
+            {/* Email */}
+            <div className="flex flex-col gap-2">
+              <p>
+                Scrivimi a:{' '}
+                <a
+                  href="mailto:smailenv91@gmail.com?subject=42-website-contact"
+                  className="link link-accent font-semibold"
+                >
+                  smailenv91@gmail.com
+                </a>
+              </p>
+
+              <p className="text-xs">
+                (Clicca per aprire il tuo client email, oppure copia
+                l&apos;indirizzo)
+              </p>
+            </div>
+
+            {/* Telefono */}
+            <div className="">
+              <p>
+                Per contatto telefonico e dettagli completi, scarica il mio CV,
+                o contattami in uno dei social qui sotto.
+              </p>
+            </div>
+
+            {/* Social */}
+            <SocialIcons />
           </div>
-
-          {/* Telefono */}
-          <div className="">
-            <p>
-              Per contatto telefonico e dettagli completi, scarica il mio CV, o
-              contattami in uno dei social qui sotto.
-            </p>
-          </div>
-
-          {/* Social */}
-          <SocialIcons />
         </div>
       </Layout>
     </>
