7b1604d9342bc08f0648a4c0dfd9c5e60759e5a9
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-10-27 13:38:58 +0100

    feat: ✨ aggiorna la pagina "About" con componenti Head e Layout
    - Aggiunto il componente Head per gestire i metadati della pagina.
    - Integrato il componente Layout per migliorare la struttura e il design della pagina "About".

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,5 +1,7 @@
 // src/routes/about.tsx
 
+import { Head } from '@/components/atoms/Head';
+import { Layout } from '@/components/molecules/Layout';
 import { createFileRoute } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/about')({
@@ -7,5 +9,17 @@ export const Route = createFileRoute('/about')({
 });
 
 function AboutPage() {
-  return <div>Prova about page</div>;
+  return (
+    <>
+      <Head
+        title="About | Smailen Vargas"
+        description="Aggiorna la descrizione"
+        keywords="Aggiungi keywords"
+        ogUrl="https://smailenvargas.com/about"
+      />
+      <Layout classContent="min-h-[calc(100vh-160px)]">
+        <div>Prova about page</div>
+      </Layout>
+    </>
+  );
 }
