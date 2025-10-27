79a1b25d429ace859ff28be210897496aeb4868f
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-10-27 14:02:20 +0100

    feat: ✨ aggiorna la pagina "About" con contenuti e separatori
    - Aggiunti sezioni con Lorem ipsum alla pagina "About" per capire lo spazio occupato.
    - Integrato il componente Separator per una migliore separazione visiva tra le sezioni.

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,6 +1,7 @@
 // src/routes/about.tsx
 
 import { Head } from '@/components/atoms/Head';
+import Separator from '@/components/atoms/Separator';
 import { Layout } from '@/components/molecules/Layout';
 import { createFileRoute } from '@tanstack/react-router';
 
@@ -17,8 +18,29 @@ function AboutPage() {
         keywords="Aggiungi keywords"
         ogUrl="https://smailenvargas.com/about"
       />
-      <Layout classContent="min-h-[calc(100vh-160px)]">
-        <div>Prova about page</div>
+      <Layout classContent="min-h-[calc(100vh-160px)] px-4">
+        <Separator />
+        <section>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+          aliquid iste facere et consequatur? Explicabo maxime magnam commodi
+          nisi, recusandae dolorum nostrum, beatae consectetur nesciunt ducimus
+          eveniet enim odit autem!
+        </section>
+        <Separator />
+        <section>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat
+          doloribus tenetur recusandae quos esse at doloremque quas repellendus,
+          accusamus nesciunt ut libero explicabo ipsam praesentium culpa.
+          Accusantium adipisci omnis perspiciatis.
+        </section>
+        <Separator />
+        <section>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo a,
+          consequuntur quis sint blanditiis ab excepturi fugit iusto tempore
+          saepe error et. Alias, aliquam. Architecto tempora voluptas
+          perspiciatis exercitationem id.
+        </section>
+        <Separator />
       </Layout>
     </>
   );
