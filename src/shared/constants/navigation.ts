28a8117826d8df2fc53a062cb1ae04ba786a2f7b
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-10-27 13:41:38 +0100

    feat: ✨ aggiungi link per la pagina "About" nella costante di navigazione
    - Aggiunto un nuovo link per la pagina "About" all'array NAVIGATION_LINKS in `navigation.ts`.

--- src/shared/constants/navigation.ts
+++ src/shared/constants/navigation.ts
@@ -2,6 +2,7 @@ export const NAVIGATION_LINKS = [
   { linkTo: '/', params: 'home' },
   { linkTo: '/projects', params: 'projects' },
   { linkTo: '/contact', params: 'contact' },
+  { linkTo: '/about', params: 'about' },
 ] as const;
 
 export type NavigationLink = (typeof NAVIGATION_LINKS)[number];
