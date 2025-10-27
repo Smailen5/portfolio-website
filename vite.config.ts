9573061fe9483fe5cb7043059af2481b3cec2cef
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-10-27 13:11:52 +0100

    build: ➕ aggiungi `@tanstack/router-plugin` nel progetto
    - Aggiornato package.json per includere `@tanstack/router-plugin` come dev dependency
    - Modificato `vite.config.ts` per integrare il plugin tanstackRouter con auto code splitting

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import tailwindcss from '@tailwindcss/vite';
+import tanstackRouter from '@tanstack/router-plugin/vite';
 import viteReact from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
@@ -6,7 +7,15 @@ import tsConfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
-  plugins: [tsConfigPaths(), viteReact(), tailwindcss()],
+  plugins: [
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
+    tsConfigPaths(),
+    viteReact(),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
