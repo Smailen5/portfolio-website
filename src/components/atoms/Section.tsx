f666af3ebb4866031329d15e7b8e63c01e196fea
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-11-03 14:42:23 +0100

    style: 💄 aggiorna il componente Section per una larghezza completa
    - Aggiunta la classe "w-full" al componente Section per garantire che occupi l'intera larghezza disponibile.

--- src/components/atoms/Section.tsx
+++ src/components/atoms/Section.tsx
@@ -1,5 +1,5 @@
 const Section = ({ children }: { children: React.ReactNode }) => {
-  return <section className="space-y-4">{children}</section>;
+  return <section className="w-full space-y-4">{children}</section>;
 };
 
 export default Section;
