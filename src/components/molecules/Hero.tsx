2a05d509e37f6c9762d0b3606af23bec824ff31c
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-11-03 14:43:21 +0100

    refactor: ♻️ migliora la struttura del componente Hero
    - Sostituito il tag <section> con il componente <Section> per una migliore semantica.
    - Aggiunti componenti <Separator> per migliorare la spaziatura visiva tra gli elementi.
    - Rimosse classi commentate non più necessarie.

--- src/components/molecules/Hero.tsx
+++ src/components/molecules/Hero.tsx
@@ -1,4 +1,6 @@
 import { Link } from '@tanstack/react-router';
+import Section from '../atoms/Section';
+import Separator from '../atoms/Separator';
 
 /**
  * Componente Hero - Sezione principale della homepage
@@ -11,16 +13,19 @@ import { Link } from '@tanstack/react-router';
  */
 export const Hero = () => {
   return (
-    <section className="w-full">
-      <main className="flex flex-col gap-4">
+    <Section>
+      <main className="">
         {/* presentazione */}
-        <header className="space-y-4">
+        <header className="">
           <p className="text-4xl">
             <span className="animate-typing text-base-content w-fit overflow-hidden border-r-2 font-mono tracking-wide whitespace-nowrap">
               {'<Hello World>'}
             </span>{' '}
             sono
           </p>
+
+          <Separator />
+
           <h2
             className="text-primary lg:tooltip text-6xl font-bold"
             data-tip="Si, il mio nome significa 'sorridi sempre'! 😊"
@@ -28,7 +33,12 @@ export const Hero = () => {
             Smailen Vargas
           </h2>
         </header>
+
+        <Separator />
+
         <p className="text-3xl">Frontend developer</p>
+
+        <Separator />
 
         {/* call to action */}
         <nav className="md:flex-star flex flex-col flex-wrap gap-4 md:flex-row">
@@ -39,7 +49,9 @@ export const Hero = () => {
             Progetti
           </Link>
         </nav>
+
+        <Separator />
       </main>
-    </section>
+    </Section>
   );
 };
