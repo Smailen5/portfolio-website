edc877739b18b3f0f14cfa46e74ba8f6e35f8df5
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-11-07 14:09:35 +0100

    style: 💄 aggiorna le dimensioni dei titoli H1 e H2 per una migliore gerarchia visiva
    - Modificata la dimensione del titolo H1 da text-2xl a text-3xl.
    - Modificata la dimensione del titolo H2 da text-xl a text-2xl.

--- src/components/atoms/heading/H2.tsx
+++ src/components/atoms/heading/H2.tsx
@@ -1,5 +1,5 @@
 export const H2 = ({ children }: { children: string }) => {
   return (
-    <h2 className="text-secondary text-xl font-bold capitalize">{children}</h2>
+    <h2 className="text-secondary text-2xl font-bold capitalize">{children}</h2>
   );
 };
