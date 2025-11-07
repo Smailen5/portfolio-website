00722cead6165662a0099d1f49f319a35d663518
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-11-07 13:37:18 +0100

    style: 💄 aggiorna il componente Strong per una migliore presentazione visiva
    - Modificato il componente Strong per includere uno sfondo accentuato e migliorare la leggibilità del testo, utilizzando un elemento <span> con classi aggiuntive.

--- src/components/atoms/Strong.tsx
+++ src/components/atoms/Strong.tsx
@@ -1,5 +1,9 @@
 const Strong = ({ children }: { children: string }) => {
-  return <strong className="text-accent">{children}</strong>;
+  return (
+    <span className="before:bg-accent relative px-1 before:absolute before:inset-0 before:-skew-x-10 before:rounded-xs before:content-['']">
+      <strong className="text-accent-content relative z-10">{children}</strong>
+    </span>
+  );
 };
 
 export default Strong;
