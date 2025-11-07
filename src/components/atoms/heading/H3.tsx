202b42f34aff6494ae3f153bef1c1c39749b3d0c
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-11-07 14:10:00 +0100

    style: 💄 aggiorna il componente H3 per una migliore presentazione visiva
    - Modificato il componente H3 per includere uno sfondo accentuato e migliorare la leggibilità del testo, utilizzando un elemento <span> con classi aggiuntive.
    - Aggiunta una classe per la capitalizzazione del testo e modificata la dimensione del titolo.

--- src/components/atoms/heading/H3.tsx
+++ src/components/atoms/heading/H3.tsx
@@ -1,3 +1,8 @@
 export const H3 = ({ children }: { children: string }) => {
-  return <h3 className="text-accent text-lg font-semibold">{children}</h3>;
+  return (
+    <h3 className="text-base-content relative inline-block text-xl font-semibold capitalize">
+      <span className="relative z-[10]">{children}</span>
+      <span className="bg-primary absolute bottom-0 left-0 h-[2px] w-full"></span>
+    </h3>
+  );
 };
