9ab86f71170f7d2858a8eb75417d96e7c4af17d3
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-11-12 11:50:45 +0100

    fix: ♿ migliora l'accessibilita' del componente Icon
    - Aggiunto aria-label per il link per fornire informazioni contestuali agli screen reader.
    - Rende decorativa l'icona impostando alt vuoto per evitare ridondanze.

--- src/components/molecules/Social/Icon.tsx
+++ src/components/molecules/Social/Icon.tsx
@@ -24,12 +24,13 @@ export const Icon: React.FC<IconType> = ({ name, icon, link }) => {
   const IconImg = icons[icon];
   return (
     <a
+      aria-label={`apri il profilo ${name} di Smailen Vargas`}
       href={link}
       target="_blank"
       rel="noopener noreferrer"
       className="group flex items-center gap-4 hover:space-x-4 lg:space-x-0"
     >
-      <img srcSet={IconImg} alt={name} className="size-5" />
+      <img srcSet={IconImg} alt="" className="size-5" />
       <span className="max-h-6 overflow-hidden font-semibold uppercase transition-all duration-700 ease-in-out group-hover:max-w-xs lg:max-w-0 lg:opacity-0 group-hover:lg:opacity-100">
         {name}
       </span>
