cb7ebc1e983ec666cd60a889a483ed7fca331c79
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-11-24 11:13:46 +0100

    style: 🎨 aggiorna lo stile della Navbar
    - Modificata la classe CSS della Navbar per includere uno sfondo semi-trasparente, un'ombra e un effetto di sfocatura sullo sfondo.

--- src/components/organisms/Navbar/Navbar.tsx
+++ src/components/organisms/Navbar/Navbar.tsx
@@ -23,7 +23,7 @@ export const Navbar = () => {
       {/* Layout */}
       <Layout>
         {/* Navbar */}
-        <nav className="fixed inset-x-0 top-0 z-50 flex items-center justify-between px-4 py-2">
+        <nav className="bg-base-200/60 fixed inset-x-0 top-0 z-50 flex items-center justify-between px-4 py-2 shadow-sm backdrop-blur-sm">
           <Avatar name="Smailen Vargas" />
 
           <div className="flex items-center gap-4">
