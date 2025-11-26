185020be63bb4d74f8d8d5c3a5f8fa4eee620912
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-11-26 11:51:07 +0100

    feat: ✨ aggiungi titolo al SkillsCarousel
    - Aggiunto un titolo "Le tecnologie che uso" al componente SkillsCarousel per migliorare la chiarezza e l'orientamento degli utenti.

--- src/components/organisms/SectionSkill/SkillsCarousel.tsx
+++ src/components/organisms/SectionSkill/SkillsCarousel.tsx
@@ -1,12 +1,15 @@
 import { SkillCard } from './SkillCard';
 // import { SkillCategory } from './SkillCategory';
 import { framework, language, utility } from '@/data/skillsData';
+import { H2 } from '@/components/atoms/heading';
 
 export const SkillsCarousel = () => {
   const allSkills = [...language, ...framework, ...utility];
   const duplicatedSkills = [...allSkills, ...allSkills];
   return (
     <>
+      <H2>Le tecnologie che uso</H2>
+
       <div className="mx-auto w-full overflow-hidden md:max-w-[976px]">
         <div className="animate-scroll flex">
           {duplicatedSkills.map((skill, index) => (
