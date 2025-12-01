7cfcfb829b622b8c3d949ffca319c536419a3173
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-12-01 13:08:43 +0100

    feat: ✨ aggiungi supporto per la proprietà textCenter nel componente H2
    - Aggiornato il componente H2 per accettare una nuova proprietà `textCenter`, che consente di centrare il testo, migliorando la flessibilità del layout.
    - Modificato il componente SkillsCarousel per utilizzare la nuova proprietà `textCenter` nel titolo.

--- src/components/atoms/heading/H2.tsx
+++ src/components/atoms/heading/H2.tsx
@@ -1,13 +1,15 @@
 export const H2 = ({
   children,
   screenReaderOnly,
+  textCenter,
 }: {
   children: string;
   screenReaderOnly?: boolean;
+  textCenter?: boolean;
 }) => {
   return (
     <h2
-      className={`text-secondary text-2xl font-bold capitalize ${screenReaderOnly && 'sr-only'}`}
+      className={`text-secondary text-2xl font-bold capitalize ${screenReaderOnly && 'sr-only'} ${textCenter && 'text-center'}`}
     >
       {children}
     </h2>
