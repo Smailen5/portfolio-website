ec8a9845a72a26e8adeeedf7fb49634cb94c680e
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2025-12-02 11:27:18 +0100

    fix: 🐛 corregge la classe di visualizzazione nel componente SkillsCarousel
    - Modificata la classe del contenitore nel componente SkillsCarousel da `flex` a `inline-flex` per garantire un comportamento di layout corretto.
    - Rimossa la proprieta' display dall'animazione.

--- src/components/organisms/SectionSkill/SkillsCarousel.tsx
+++ src/components/organisms/SectionSkill/SkillsCarousel.tsx
@@ -1,7 +1,7 @@
 import { SkillCard } from './SkillCard';
 // import { SkillCategory } from './SkillCategory';
-import { framework, language, utility } from '@/data/skillsData';
 import { H2 } from '@/components/atoms/heading';
+import { framework, language, utility } from '@/data/skillsData';
 
 export const SkillsCarousel = () => {
   const allSkills = [...language, ...framework, ...utility];
@@ -11,7 +11,7 @@ export const SkillsCarousel = () => {
       <H2 textCenter>Le tecnologie che uso</H2>
 
       <div className="mx-auto w-full overflow-x-hidden md:max-w-[976px]">
-        <div className="animate-scroll flex">
+        <div className="animate-scroll inline-flex">
           {duplicatedSkills.map((skill, index) => (
             <div
               key={`${skill.name}-${index}`}
