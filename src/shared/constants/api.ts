f4b486543b4336c4bb2786e35e2015ca14cb05f9
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2026-07-30 10:53:02 +0200

    chore: migra api.ts da URL hardcoded
    - migra api.ts da hardcoded a variabili d'ambiente

--- src/shared/constants/api.ts
+++ src/shared/constants/api.ts
@@ -1,11 +1,3 @@
-export const ENDPOINTS = {
-  GITHUB: {
-    CDN: {
-      PROJECTS: 'https://portfolio-cdn.netlify.app/projects.json',
-    },
-    README: {
-      FOLDER:
-        'https://github.com/Smailen5/Frontend-Mentor-Challenge/tree/main/packages/',
-    },
-  },
-};
+export const API_URL = import.meta.env.VITE_API_URL;
+export const CDN_URL = import.meta.env.VITE_NETLIFY_CDN_URL;
+export const GITHUB_README_FOLDER = import.meta.env.VITE_GITHUB_README_FOLDER;
