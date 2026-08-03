eee90d356344523714e71c6029a93bf29052f210
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2026-08-03 13:58:05 +0200

    fix: sostituisce `srcSet` con `src`

--- src/components/molecules/Social/Icon.tsx
+++ src/components/molecules/Social/Icon.tsx
@@ -30,7 +30,7 @@ export const Icon: React.FC<IconType> = ({ name, icon, link }) => {
       rel="noopener noreferrer"
       className="group flex items-center gap-4 hover:space-x-4 lg:space-x-0"
     >
-      <img srcSet={IconImg} alt="" className="size-5" />
+      <img src={IconImg} alt="" className="size-5" />
       <span className="max-h-6 overflow-hidden font-semibold uppercase transition-all duration-700 ease-in-out group-hover:max-w-xs lg:max-w-0 lg:opacity-0 group-hover:lg:opacity-100">
         {name}
       </span>
