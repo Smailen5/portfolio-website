795a98ed91b31d11b04bef2d64183d8dd0801405
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2026-08-07 16:43:55 +0200

    fix: rinomina params in label
    - rinomina la chiave passata come parametro al Link di tanstack
    - migliora la semantica

--- src/components/organisms/Navbar/Navbar.tsx
+++ src/components/organisms/Navbar/Navbar.tsx
@@ -56,11 +56,11 @@ export const Navbar = () => {
               <nav className="flex items-center gap-4">
                 {NAVIGATION_LINKS.map(link => (
                   <Link
-                    key={link.params}
+                    key={link.label}
                     to={link.linkTo}
                     className="capitalize"
                   >
-                    {link.params}
+                    {link.label}
                   </Link>
                 ))}
                 <CurriculumDownload />
