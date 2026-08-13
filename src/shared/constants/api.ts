8d909b68ee65a8798fc188e7e2c759333597cae7
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2026-08-13 20:08:57 +0200

    refactor: rimuove GITHUB_README_FOLDER dal codice

--- src/shared/constants/api.ts
+++ src/shared/constants/api.ts
@@ -1,3 +1,2 @@
 export const API_URL = import.meta.env.VITE_API_URL;
 export const CDN_URL = import.meta.env.VITE_NETLIFY_CDN_URL;
-export const GITHUB_README_FOLDER = import.meta.env.VITE_GITHUB_README_FOLDER;
