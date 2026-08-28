2aef28652c8e579e2f118d31a524f64ff96daf2c
Smailen Vargas <126914694+Smailen5@users.noreply.github.com>
2026-08-28 10:52:49 +0200

    fix: rimuove slice no-op in getInitials (#230)

--- src/shared/utils/getInitials.ts
+++ src/shared/utils/getInitials.ts
@@ -1,8 +1,6 @@
 export const getInitials = (name: string) => {
   if (!name) return '';
   const words = name.split(' ');
-  const initials = words
-    .slice(0, words.length)
-    .map(word => word.charAt(0).toUpperCase());
+  const initials = words.map(word => word.charAt(0).toUpperCase());
   return initials.join('');
 };
