649a3e5dd2df2bd526b430ef6dd9aa781ff3c298
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2026-08-31 15:35:07 +0200

    feat: aggiunge callback al componente CardError

--- src/components/molecules/CardError.tsx
+++ src/components/molecules/CardError.tsx
@@ -1,10 +1,16 @@
-export const CardError = () => {
+interface CardErrorProps {
+  onRetry: () => void;
+}
+
+export const CardError = ({ onRetry }: CardErrorProps) => {
   return (
     <div className="bg-base-300 flex w-full flex-col items-center rounded-2xl p-4">
       <p className="mb-4">
         Errore nel recupero dei progetti. Riprova piu&apos; tardi.
       </p>
-      <button className="btn btn-accent">Riprova</button>
+      <button className="btn btn-accent" onClick={onRetry}>
+        Riprova
+      </button>
     </div>
   );
 };
