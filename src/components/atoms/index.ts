fb2dac127ef0556303d68033a6620af2a0bc0a40
Smailen5 <126914694+Smailen5@users.noreply.github.com>
2026-09-09 17:09:43 +0200

    refactor: aggiunge nuovi import barrel con export

---
+++ src/components/atoms/index.ts
@@ -0,0 +1,5 @@
+export { Avatar } from './Avatar';
+export { Head } from './Head';
+export { Section } from './Section';
+export { Separator } from './Separator';
+export { Strong } from './Strong';
---
+++ src/components/molecules/index.ts
@@ -0,0 +1,7 @@
+export { CardError } from './CardError';
+export { CardSkeleton } from './CardSkeleton';
+export { Hero } from './Hero';
+export { Layout } from './Layout';
+export { Presentation } from './Presentation';
+export { SocialIcons } from './SocialIcons';
+export { ToggleTheme } from './ToggleTheme';
